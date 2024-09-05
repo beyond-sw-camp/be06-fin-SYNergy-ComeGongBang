efd9c36298de015ee7f3f7a8108eb4d0052ef928
706com <ehdgusjdbswl@gmail.com>
2024-09-05 23:44:14 +0900

    [Init] 최초 프로젝트를 위한 frontend 보일러 플레이트 세팅
    pinia, router 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-Vue.use(VueRouter);
+import { createPinia } from 'pinia';
+import piniaPersistedState from 'pinia-plugin-persistedstate'
 
-createApp(App).mount('#app')
+import router from './router';
+
+const pinia = createPinia();
+pinia.use(piniaPersistedState);
+
+const app = createApp(App);
+
+app.use(pinia);
+app.use(router);
+app.mount('#app')
