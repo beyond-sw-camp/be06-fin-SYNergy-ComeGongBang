28a6692a5ef85ae5cb2bbefa72795e933f1f2d45
postrel63 <postrel63@naver.com>
2024-09-06 23:26:23 +0900

    [Feat] 배송지 목록 조회, 기본 배송지 조회, 배송지 추가 #41
    spring security 설정이 아직 없어서 유저는 임의로 생성
    + global exception

--- backend/src/main/java/com/synergy/backend/member/repository/MemberRepository.java
+++ backend/src/main/java/com/synergy/backend/member/repository/MemberRepository.java
@@ -1,4 +1,9 @@
 package com.synergy.backend.member.repository;
 
-public class MemberRepository {
+import com.synergy.backend.member.model.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
--- backend/src/main/java/com/synergy/backend/member/service/MemberService.java
+++ backend/src/main/java/com/synergy/backend/member/service/MemberService.java
@@ -1,4 +1,71 @@
 package com.synergy.backend.member.service;
 
+import com.synergy.backend.common.BaseException;
+import com.synergy.backend.common.BaseResponseStatus;
+import com.synergy.backend.member.model.entity.DeliveryAddress;
+import com.synergy.backend.member.model.entity.Member;
+import com.synergy.backend.member.model.request.createDeliveryAddressReq;
+import com.synergy.backend.member.model.response.deliveryAddressRes;
+import com.synergy.backend.member.repository.DeliveryAddressRepository;
+import com.synergy.backend.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final DeliveryAddressRepository deliveryAddressRepository;
+    private final MemberRepository memberRepository;
+
+    public deliveryAddressRes getDefaultDeliveryAddress(Long userIdx) throws BaseException {
+        Member member = getMember(userIdx);
+        return deliveryAddressRes.from(member.getDefaultAddress(), true);
+    }
+
+    public List<deliveryAddressRes> getDeliveryAddressList(Long userIdx) throws BaseException {
+
+        Member member = getMember(userIdx);
+        List<DeliveryAddress> allByMemberIdx = deliveryAddressRepository.getAllByMemberIdx(userIdx);
+
+        // 기본 배송지이면 isDefault = true, 리스트 맨 위에 존재하게
+        Deque<deliveryAddressRes> result = new LinkedList<>();
+
+        for (DeliveryAddress deliveryAddress : allByMemberIdx) {
+            Boolean isDefault = (deliveryAddress == member.getDefaultAddress());
+            if (isDefault) {
+                result.addFirst(deliveryAddressRes.from(deliveryAddress, true));
+            } else {
+                result.add(deliveryAddressRes.from(deliveryAddress, false));
+            }
+        }
+        return new ArrayList<>(result);
+    }
+
+    @Transactional
+    public void createDeliveryAddress(createDeliveryAddressReq req, Long userIdx) throws BaseException {
+        //필수값 필수!
+        if (req.getAddress() == null || req.getRecipient() == null || req.getCellPhone() == null) {
+            throw new BaseException(BaseResponseStatus.REQUIRED_VALUE_NOT_ENTERED);
+        }
+        Member member = getMember(userIdx);
+        DeliveryAddress saved = deliveryAddressRepository.save(req.toEntity(member));
+
+        //기본 배송지이면 설정
+        if (req.getIsDefault()) {
+            member.updateDefaultAddress(saved);
+            memberRepository.save(member);
+        }
+    }
+
+    private Member getMember(Long userIdx) throws BaseException {
+        return memberRepository.findById(userIdx).orElseThrow(() ->
+                new BaseException(BaseResponseStatus.NOT_FOUND_USER));
+    }
 }
