007815eac8b14a3f892005e3976a8ea6eeaf9f60
706com <ehdgusjdbswl@gmail.com>
2024-09-08 18:03:27 +0900

    [Feat] 이메일을 통한 엔티티 조회 추가 (#4)

--- backend/src/main/java/com/synergy/backend/member/repository/MemberRepository.java
+++ backend/src/main/java/com/synergy/backend/member/repository/MemberRepository.java
@@ -1,7 +1,10 @@
 package com.synergy.backend.member.repository;
 
 import com.synergy.backend.member.model.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
 }
