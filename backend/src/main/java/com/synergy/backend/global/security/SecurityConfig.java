10270a897520a5873e723cc51cb1499d70ae03ac
706com <ehdgusjdbswl@gmail.com>
2024-09-08 18:04:22 +0900

    [Feat] 로그인 필터를 시큐리티 내부에 등록 (#4)

--- backend/src/main/java/com/synergy/backend/global/security/SecurityConfig.java
+++ backend/src/main/java/com/synergy/backend/global/security/SecurityConfig.java
@@ -1,19 +1,32 @@
 package com.synergy.backend.global.security;
 
+import com.synergy.backend.global.security.filter.LoginFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
@@ -24,6 +37,9 @@ public class SecurityConfig {
         http.authorizeHttpRequests((auth)->
                 auth.anyRequest().permitAll()   // 일시적 모두 허용
         );
+
+        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)),
+                UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
