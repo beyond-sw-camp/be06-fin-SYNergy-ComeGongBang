2091febc9cc7246153b3f29bec1c1c679a09c9f9
706com <ehdgusjdbswl@gmail.com>
2024-09-08 15:20:27 +0900

    [Refactor] 빌더생성자 추가, 생년월일 타입 변경

--- backend/src/main/java/com/synergy/backend/member/model/entity/Member.java
+++ backend/src/main/java/com/synergy/backend/member/model/entity/Member.java
@@ -2,10 +2,14 @@ package com.synergy.backend.member.model.entity;
 
 import com.synergy.backend.common.model.BaseEntity;
 import jakarta.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "member")
+@NoArgsConstructor
 public class Member extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,7 +19,7 @@ public class Member extends BaseEntity {
     private String nickname;
     private String cellPhone;
     private LocalDateTime joinDate;
-    private LocalDateTime birthday;
+    private LocalDate birthday;
     private String gender;
     private Boolean emailAuthentication;
     private String role;
@@ -27,4 +31,23 @@ public class Member extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "default_address_idx")
     private DeliveryAddress defaultAddress;
+
+    @Builder
+    public Member(String email, String password, String nickname, String cellPhone, LocalDateTime joinDate,
+                  LocalDate birthday, String gender,
+                  String profileImageUrl,
+                  DeliveryAddress defaultAddress) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.cellPhone = cellPhone;
+        this.joinDate = joinDate;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.emailAuthentication = false;   // 이메일 인증은 받지 못한 상태
+        this.role = "USER";     // 최초 생성 USER
+//        this.profileImageUrl = profileImageUrl;   // 프로필 이미지는 수정할때만
+//        this.grade = "아기손"; //등급 최저
+//        this.defaultAddress = defaultAddress; //기본 배송지
+    }
 }
