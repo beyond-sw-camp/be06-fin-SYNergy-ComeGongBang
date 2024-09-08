35e1253f33c8f8354bd1778d67b338d5c23a66ef
706com <ehdgusjdbswl@gmail.com>
2024-09-08 15:21:21 +0900

    [Feat] 회원가입 api 구현

--- backend/src/main/java/com/synergy/backend/member/controller/MemberController.java
+++ backend/src/main/java/com/synergy/backend/member/controller/MemberController.java
@@ -1,4 +1,27 @@
 package com.synergy.backend.member.controller;
 
+import com.synergy.backend.member.model.request.MemberSignupReq;
+import com.synergy.backend.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/member")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody MemberSignupReq memberSignupReq){
+        String result = memberService.signup(memberSignupReq);
+        return ResponseEntity.ok(result);
+    }
+
+
 }
