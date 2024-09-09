029d0d1dc0ca3f49ae2d542c0354d1fcc1515adc
postrel63 <postrel63@naver.com>
2024-09-09 11:09:45 +0900

    [Feat] daum api 사용 script #68
    더블쿼터 -> 싱글쿼터

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,19 @@
-import { createRouter, createWebHistory } from "vue-router";
-import App from "@/App";
+import { createRouter, createWebHistory } from 'vue-router';
+import App from '@/App';
+import DeliveryComponent from '@/components/member/DeliveryComponent.vue';
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path: "/", component: App },
-    ],
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: App,
+    },
+    {
+      path: '/deliveryAddress',
+      component: DeliveryComponent,
+    },
+  ],
 });
 
 export default router;
