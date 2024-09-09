30a7c343499697fdb6d1dfe0e202de5e5184d26c
706com <ehdgusjdbswl@gmail.com>
2024-09-10 01:06:32 +0900

    [Feat] 로그인 페이지 router-link 추가

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,12 +2,16 @@ import { createRouter, createWebHistory } from "vue-router";
 import App from "@/App";
 // import ProductComponent from "@/components/product/ProductComponent"
 import ProductListComponent from "@/components/product/ProductListComponent"
+import LoginPage from "@/pages/member/LoginPage.vue"
+import SignupPage from "@/pages/member/SingupPage.vue"
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: "/", component: App },
         { path: "/product", component : ProductListComponent},
+        { path: "/login", component : LoginPage},
+        { path: "/signup", component: SignupPage},
     ],
 });
 
