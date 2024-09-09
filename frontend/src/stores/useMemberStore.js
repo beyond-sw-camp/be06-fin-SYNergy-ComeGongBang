858fb2f97be30153b46c8776d32637c7fe2bb31a
706com <ehdgusjdbswl@gmail.com>
2024-09-10 01:09:20 +0900

    [Feat] 회원 관련 스토어 생성 및 기능 구현 (#48)

--- frontend/src/stores/useMemberStore.js
+++ frontend/src/stores/useMemberStore.js
@@ -7,12 +7,12 @@ export const useMemberStore = defineStore('member', {
             userIdx : "",
             userName : "",
             password:"",
-            userEmail : "",
+            email : "",
             isLogined : false,
             uuid:"",
-
-            result : false
         },
+
+        status : 0, 
 
         electedMyCategories: [],  // 빈 배열로 초기화
         selectedLikeCategories: [],  // 빈 배열로 초기화
@@ -27,61 +27,62 @@ export const useMemberStore = defineStore('member', {
         async login(member){
             console.log(member.email);
             console.log(member.password);
-            this.result = true;
-            return this.result;
-            // let url = `/api/member/login`;
 
-            // let response = await axios.post(url, member, {withCredentials:false}); //응답 받아서 저장
-            // if(response.data.isSuccess){
-            //     this.member.isLogined=true;
-            //     this.member.userIdx=response.data.idx;
-            //     this.member.userName=response.data.nickname;
-            //     this.member.userEmail=response.data.email;
-            //     this.member.isLogined=true;
+            let url = `/api/login`;
+            let response = await axios.post(url, member, {withCredentials:false}); //응답 받아서 저장
 
-            //     if(response.data.firstLogin){
-            //         let newMember = {
-            //             idx:this.member.userIdx,
-            //             nickname:this.member.userName,
-            //             firstLogin:false
-            //         }
-            //         this.modify(newMember);
-            //     }
-            // }
-            // return response.data.firstLogin;
+            console.log(response);
+            
+            if(response.status === 200){
+                this.member.isLogined=true;
+                this.member.userIdx=response.data.idx;
+                this.member.userName=response.data.nickname;
+                this.member.userEmail=response.data.email;
+
+                console.log(this.member);
+            }
+            return this.member.isLogined;
         },
+
         async signup(member){
-            let url = '/proxy/member/signup';
+            let url = '/api/member/signup';
 
             let response = await axios.post(url, member, {withCredentials:false});
             console.log(response);
         },
-        async sendEmail(email){
+
+        async emailRequest(email){
+            console.log(email);
+            this.member.email = email;
+
+            let emailAuthReq = {
+                email : email,
+                uuid : "",
+            }
+            let url = `/api/email/request`
+            let response = await axios.post(url, emailAuthReq);
+            this.status = response.status;
+
+            console.log("aaaaa" + this.status);
+            return this.status;
+        },
+
+        async verifyUuid(email, uuid){
+            
+            this.member.uuid= uuid;
 
             let emailAuthReq={
-                email:email,
-                uuid:""
+                email: email,
+                uuid: uuid,
             };
 
-            // console.log(emailAuthReq);
-
-            let url = `/proxy/email/send`
+            let url = `/api/email/verify`;
 
             let response = await axios.post(url, emailAuthReq);
             console.log(response);
+            return response;
         },
-        async verify(member){
 
-            let emailAuthReq={
-                email:member.email,
-                uuid:member.uuid
-            }
-
-            let url = `/proxy/email/verify`;
-
-            let response = await axios.post(url, emailAuthReq);
-            console.log(response);
-        },
         logout() {
             this.member.isLogined = false;
             alert("로그아웃이 완료되었습니다.");
