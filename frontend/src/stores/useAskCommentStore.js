a79fc0ebd68730ea46ca98634114b7f4a1ccc2da
choijw1116 <choijw1116@gmail.com>
2024-09-10 09:57:37 +0900

    [Feat] Create useAskCommentStore file(create read,create methods in store files)

--- frontend/src/stores/useAskCommentStore.js
+++ frontend/src/stores/useAskCommentStore.js
@@ -5,48 +5,60 @@ export const useAskCommentStore = defineStore('askComment', {
     state: () => ({
         request: {
             productIdx: 0,
-            content: "",
-            userProfileImg: "",
-            atelierProfileImg: "",
             askCommentListAll: [],//문의목록리스트
+            totalListCount: 0, //전체목록리스트갯수
+            isSecret: 0,    //비밀댓글유무
+
+            currentPage: 0,   // 현재 페이지 번호
+            pageSize: 10,     // 페이지당 받을 아이템 수 
+            hasMore: true,      //리스트가 더 남아있는지 여부
+
         },
 
     }),
     actions: {
         // 문의작성
-        async createAskComment() {
-            const url = '/proxy/ask/comment/create';
-
+        async createAskComment(textData) {
+            // const url = 'https://d792dcd9-e7f7-4d13-9b15-ec2699a9e775.mock.pstmn.io/ask/comment/create';
+            const url = 'proxy/ask/comment/create';
             const req = {
                 idx: this.request.productIdx,
-                content: this.request.content,
-            };
-
+                content: textData,
+                isSecret: this.isSecret
+            }
             try {
-                // console.log(req);
                 const response = await axios.post(url, req);
-                console.log('Post created successfully:', response.data);
-                // if (response.data.code === 2101) {
-                //     alert(response.data.message);
-                // } else if (response.data.code === 2102) {
-                //     alert(response.data.message);
-                // }
+                console.log(response)
             } catch (error) {
                 console.error('Error creating post:', error);
             }
         },
 
         //문의 목록 조회
-        async readAllAskCommentList() {
-            let url = `/proxy/ask/comment/read?idx=${productIdx}`;
-            const response = await axios.get(url);
-            try {
-                this.askCommentListAll = response.data.result;
-                // console.log(response.data.result);
+        //페이징처리
+        async readAllAskCommentList(page, size) {
+            if (this.hasMore) {
+                try {
+                    let url = `proxy/ask/comment/read?idx=${this.request.productIdx}&page=${page}&size=${size}`;
+                    // let url = `https://d792dcd9-e7f7-4d13-9b15-ec2699a9e775.mock.pstmn.io/ask/comment/read?idx=${this.request.productIdx}`;
+                    const response = await axios.get(url);
 
-            } catch (error) {
-                console.log(error);
+                    //받아온 데이터의 길이가 size보다 작을때
+                    if (response.data.result.length < size) {
+                        this.hasMore = false;
+                    }
+
+                    //더보기를 누르면 보여줄 아이템이 쌓이므로 객체에 담아준다.
+                    this.askCommentListAll = [...response.data.result, ...this.askCommentListAll]
+                    this.currentPage++;
+                    console.log("store에서 commentList", this.askCommentListAll)
+
+                } catch (error) {
+                    console.log(error);
+                }
             }
+
         }
     }
 })
+
