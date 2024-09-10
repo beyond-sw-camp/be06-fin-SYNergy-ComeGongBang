a382545537a34a5961cae699c09028b227ba4585
choijw1116 <choijw1116@gmail.com>
2024-09-10 09:56:08 +0900

    [Feat] Create useAskCommentComponent(data mapping, paging)

--- frontend/src/components/AskCommentComponent.vue
+++ frontend/src/components/AskCommentComponent.vue
@@ -16,7 +16,8 @@
           <span class="subtitle1_bold_small">댓글</span>
         </div>
       </div>
-      <!---------------------------이전댓글보기버튼----------------------------->
+      <!---------------------------댓글보기버튼----------------------------->
+      <!--  -->
       <button
         type="outline"
         class="CoreButton CoreButton--block BaseButtonRectangle body1-bold-small BaseButtonRectangle__outline mb-[12px]"
@@ -30,20 +31,27 @@
         "
         data-v-524f63ea=""
         data-v-7940d6dd=""
+        v-if="
+          askCommentStore &&
+          askCommentStore.askCommentListAll &&
+          this.askCommentStore.hasMore
+        "
+        @click="loadData"
       >
         <div class="inline-flex items-center" data-v-524f63ea="">
-          <span class="CoreButton__text" data-v-524f63ea="">
-            이전 댓글 더보기
-          </span>
+          <span class="CoreButton__text" data-v-524f63ea=""> 댓글 더보기 </span>
         </div>
-        <!--]-->
       </button>
       <!--------------------------댓글리스트----------------------------------------------->
-      <!--data.isSecret = 0 일때 댓글보이기-->
-      <div v-if="isSecret === 0">
+      <div
+        v-for="items in this.askCommentStore.askCommentListAll"
+        :key="items.idx"
+        :item="items"
+      >
         <div data-v-cb37c401="" class="CommentItem">
           <div data-v-cb37c401="" class="CommentItem__inner">
             <!------------------ 댓글 단 유저의 프로필이미지----------------------->
+            <!-- items.profileImageUrl 로 변경하기 -->
             <div
               data-v-2fc5c54e=""
               data-v-cb37c401=""
@@ -69,9 +77,7 @@
                   --CoreBadge-offset-y: 0;
                   --CoreBadge-position: relative;
                 "
-              >
-                <!-------------------------------->
-              </div>
+              ></div>
               <div
                 data-v-8f163f81=""
                 data-v-2fc5c54e=""
@@ -90,14 +96,14 @@
             <!-------------------유저가 단 문의댓글------------------------------>
             <div data-v-cb37c401="" class="CommentItem__innerContents">
               <div data-v-cb37c401="" class="CommentItem__innerContentsName">
-                <span data-v-cb37c401="">이지수</span>
+                <span data-v-cb37c401="">{{ items.username }}</span>
               </div>
-              <div v-if="isSecret === 0">
+              <div v-if="items.isSecret === 0">
                 <div
                   data-v-cb37c401=""
                   class="CommentItem__innerContentsComment"
                 >
-                  요 문구들오 랩탑 리무버블 스티커도 판매되면 좋겠어요!ㅎㅎ
+                  {{ items.content }}
                 </div>
               </div>
               <div v-else>
@@ -115,6 +121,7 @@
             data-v-c6b48237=""
             data-v-cb37c401=""
             class="CommentItemReply ml-[40px] mt-[4px]"
+            v-if="items.replies"
           >
             <div
               data-v-9783a2c9=""
@@ -131,7 +138,8 @@
               <div data-v-9783a2c9="" class="BaseArrow__arrow"></div>
             </div>
             <div data-v-c6b48237="" class="CommentItemReply__contents">
-              <!--주인장 프로필 이미지 & 이름-->
+              <!-----------주인장 프로필 이미지 & 이름----------------->
+              <!-- items.replies.profileImageUrl 로 변경 -->
               <div data-v-c6b48237="" class="CommentItemReply__contentsUser">
                 <div
                   data-v-2fc5c54e=""
@@ -185,17 +193,17 @@
                   data-v-c6b48237=""
                   class="CommentItemReply__contentsUserName"
                 >
-                  {{ atelier.name }}
+                  {{ items.replies.ateliername }}
                 </div>
               </div>
               <!-- 주인장 답변 내용 -->
 
               <div
-                v-if="isSecret === 0"
+                v-if="items.isSecret === 0"
                 data-v-c6b48237=""
                 class="CommentItemReply__contentsComment"
               >
-                {{ atelier.content }}
+                {{ items.replies.content }}
               </div>
               <div v-else>비밀댓글입니다.</div>
             </div>
@@ -239,24 +247,64 @@
                 data-v-be77401d=""
                 style="height: 1px"
                 v-model="textData"
-                @click="sendContentToServer"
+                @input="checkTextLength"
               ></textarea>
             </div>
             <!----------------댓글 등록버튼---------------------->
             <button
               type="default"
-              class="CoreButton CoreButton--disabled BaseButtonText body1-regular-small BaseButtonText--disabled BaseSend__button"
               style="
                 background-color: transparent;
                 color: black;
                 height: 39px;
                 flex-direction: row-reverse;
+                justify-content: center;
+                display: block;
                 --core-button-padding-x: 8;
+                --core-button-padding-y: 8;
                 font-weight: bold;
+                border: 1px solid lightgray;
+                border-radius: 10px;
+                padding: 10px;
+                padding-right: 18px;
               "
               data-v-524f63ea=""
               data-v-8493c3f2=""
               data-v-04f97765=""
+              :class="
+                isButtonActive == true
+                  ? 'CoreButton BaseButtonText body1-regular-small BaseSend__button active'
+                  : 'CoreButton--disabled BaseButtonText--disabled BaseButtonText body1-regular-small BaseSend__button'
+              "
+              @click="sendSecretToServer"
+            >
+              <div class="inline-flex items-center" data-v-524f63ea="">
+                <div class="CoreButton__text" data-v-524f63ea="">비밀댓글</div>
+              </div>
+            </button>
+            <button
+              type="default"
+              style="
+                background-color: transparent;
+                color: black;
+                height: 39px;
+                flex-direction: row-reverse;
+                font-weight: bold;
+                border: 1px solid lightgray;
+                border-radius: 10px;
+                /* margin: 0px 10px 0px 10px; */
+                padding: 0px 13px 0px 10px;
+                --core-button-padding-x: 8;
+              "
+              data-v-524f63ea=""
+              data-v-8493c3f2=""
+              data-v-04f97765=""
+              :class="
+                isButtonActive == true
+                  ? 'CoreButton BaseButtonText body1-regular-small BaseSend__button'
+                  : 'CoreButton--disabled BaseButtonText--disabled BaseButtonText body1-regular-small BaseSend__button'
+              "
+              @click="sendContentToServer"
             >
               <div class="inline-flex items-center" data-v-524f63ea="">
                 <span class="CoreButton__text" data-v-524f63ea=""> 등록 </span>
@@ -270,38 +318,87 @@
 </template>
 
 <script>
-import { useAskCommentStore } from "@/store/useAskCommentStore";
+import { useAskCommentStore } from "../stores/useAskCommentStore.js";
 import { mapStores } from "pinia";
+import { ref } from "vue";
 
 export default {
   name: "AskCommentComponent",
-  components: {
-    MyPostShareItemComponent,
-  },
+  components: {},
   data() {
-    return {};
+    return {
+      textData: ref(""), //ref로 선언
+      commentList: [],
+      isButtonActive: false, // 버튼 활성화 상태
+      isSecret: [],
+    };
   },
   computed: {
     ...mapStores(useAskCommentStore),
   },
   created() {},
-  mounted() {
-    this.readAllAskCommentList();
-    this.useAskCommentStore.askCommentListAll;
+  async mounted() {
+    // 스토어의 메서드 호출
+    await this.askCommentStore.readAllAskCommentList();
+    this.checkTextLength(); // 페이지 로드 시 버튼 상태 확인
+    console.log(
+      "mounted에서 목록리스트확인 :" +
+        this.askCommentStore.askCommentListAll.length
+    ); // 스토어 확인
+
+    this.loadData();
+    console.log(this.askCommentStore.askCommentListAll.length);
   },
+  updated() {},
   methods: {
-    //textarea의 data를 서버에 보내기
-    sendContentToServer() {
-      this.useAskCommentStore.sendData(textData.value);
+    // textarea의 글자 수 체크
+    checkTextLength() {
+      if (this.textData.length >= 3) {
+        this.isButtonActive = true;
+      } else this.isButtonActive = false;
+      console.log("isButtonActive 상태:", this.isButtonActive);
     },
 
-    //목록리스트
-    async getAskCommentList() {
-      await this.myPostStore.getMyJoinShareListAll();
+    //textarea의 data를 서버에 보내기
+    async sendContentToServer() {
+      if (!this.isButtonActive) return; // 비활성화 상태에서는 전송하지 않음
+      try {
+        await this.askCommentStore.createAskComment(this.textData);
+        this.textData = ""; //입력 후 초기화
+        this.isButtonActive = false; // 초기화 후 버튼 비활성화
+        !this.askCommentStore.isSecret;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    //비밀댓글
+    async sendSecretToServer() {
+      if (!this.isButtonActive) return;
+      try {
+        if (this.askCommentStore.isSecret == 1) {
+          this.askCommentStore.isSecret = 0;
+          alert("비밀댓글이 취소되었습니다");
+        } else {
+          this.askCommentStore.isSecret = 1;
+          alert("비밀댓글이 설정되었습니다.");
+        }
+
+        console.log(this.askCommentStore.isSecret);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    //댓글더보기
+    async loadData() {
+      this.askCommentStore.readAllAskCommentList(
+        this.askCommentStore.currentPage,
+        this.askCommentStore.pageSize
+      );
     },
   },
 };
 </script>
-
 <style scoped>
 </style>
