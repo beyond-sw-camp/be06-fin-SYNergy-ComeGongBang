98093cec0baa806918f594d3d51eeb1aa0eae6f5
postrel63 <postrel63@naver.com>
2024-09-10 12:40:10 +0900

    [Feat] 내 장바구니 모든 정보 조회 query  #81

--- backend/src/main/java/com/synergy/backend/orders/repository/CartRepository.java
+++ backend/src/main/java/com/synergy/backend/orders/repository/CartRepository.java
@@ -1,7 +1,35 @@
-package com.synergy.backend.orders.repository;
+    package com.synergy.backend.orders.repository;
 
-import com.synergy.backend.orders.model.entity.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
+    import com.synergy.backend.orders.model.entity.Cart;
+    import com.synergy.backend.orders.model.response.CartDTO;
+    import org.springframework.data.jpa.repository.JpaRepository;
+    import org.springframework.data.jpa.repository.Query;
 
-public interface CartRepository extends JpaRepository<Cart, Long> {
-}
+    import java.util.List;
+
+    public interface CartRepository extends JpaRepository<Cart, Long> {
+
+        @Query("SELECT new com.synergy.backend.orders.model.response.CartDTO(" +
+                "    c.idx, " +
+                "    o.idx, " +
+                "    o.majorOption.idx, " +
+                "    o.majorOption.name, " +
+                "    o.subOption.idx, " +
+                "    o.subOption.name, " +
+                "    c.count, " +
+                "    c.price, " +
+                "    p.name, " +
+                "    p.idx, " +
+                "    a.name, " +
+                "    a.idx )" +
+                "FROM OptionInCart o " +
+                "JOIN o.cart c " +
+                "JOIN c.product p " +
+                "JOIN p.atelier a " +
+                "WHERE c.idx IN (" +
+                "    SELECT c2.idx " +
+                "    FROM Cart c2 " +
+                "    WHERE c2.member.idx = :userIdx" +
+                ")")
+        List<CartDTO> findByUserIdx(Long userIdx);
+    }
