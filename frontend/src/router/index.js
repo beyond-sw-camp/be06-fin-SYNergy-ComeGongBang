1dd982935985e4f5b3d79cfaa4581cc814a50ef3
choijw1116 <choijw1116@gmail.com>
2024-09-11 11:40:47 +0900

    [Style] delete gap from index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,36 +1,20 @@
-import {createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import App from "@/App";
 // import ProductComponent from "@/components/product/ProductComponent"
 import ProductListComponent from "@/components/product/ProductListComponent"
 import DeliveryComponent from '@/components/member/DeliveryComponent.vue';
 import Carousel from '@/components/product/ProductCarouselComponent';
+import AskCommentComponent from "@/components/AskCommentComponent.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {
-            path: "/",
-            component: App
-        },
-
-        {
-            path: "/product",
-            component: ProductListComponent
-        },
-
-        {
-            path: '/',
-            component: App,
-        },
-
-        {
-            path: '/deliveryAddress',
-            component: DeliveryComponent,
-        },
-        {
-            path: '/carousel',
-            component: Carousel
-        }
+        { path: "/", component: App },
+        { path: "/product", component: ProductListComponent },
+        { path: '/', component: App },
+        { path: '/deliveryAddress', component: DeliveryComponent },
+        { path: '/carousel', component: Carousel },
+        { path: '/ask', component: AskCommentComponent }
     ],
 });
 
