e2db03a24d079f6cb71591811be0b2def74b7fc1
706com <ehdgusjdbswl@gmail.com>
2024-09-12 17:02:20 +0900

    [Feat] 회원 정보 조회 페이지 구현 (#51)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,6 +5,7 @@ import LoginPage from "@/pages/member/LoginPage.vue"
 import SignupPage from "@/pages/member/SingupPage.vue"
 import DeliveryComponent from '@/components/member/DeliveryComponent.vue';
 import Carousel from '@/components/product/CarouselComponent';
+import MemberInfoPage from "@/pages/member/MemberInfoPage.vue";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -14,7 +15,8 @@ const router = createRouter({
         { path: "/login", component : LoginPage},
         { path: "/signup", component: SignupPage},
         { path: '/deliveryAddress', component: DeliveryComponent },
-        { path: '/carousel', component: Carousel }
+        { path: '/carousel', component: Carousel },
+        { path: '/member/info', component : MemberInfoPage},
     ],
 });
 
