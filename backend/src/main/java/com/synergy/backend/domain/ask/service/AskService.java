fbcbeb32254f322e6c51caf92f4209e23b8e0c06
choijw1116 <choijw1116@gmail.com>
2024-09-14 10:29:39 +0900

    [Feat] 문의생성,조회기능 수정 및 페이징처리추가

--- backend/src/main/java/com/synergy/backend/domain/ask/service/AskService.java
+++ backend/src/main/java/com/synergy/backend/domain/ask/service/AskService.java
@@ -1,5 +1,6 @@
 package com.synergy.backend.domain.ask.service;
 
+import com.synergy.backend.domain.product.model.dto.response.SearchProductRes;
 import com.synergy.backend.global.exception.BaseException;
 import com.synergy.backend.global.common.BaseResponseStatus;
 import com.synergy.backend.domain.ask.model.entity.Ask;
@@ -14,11 +15,17 @@ import com.synergy.backend.domain.ask.model.entity.Reply;
 
 import com.synergy.backend.domain.ask.model.response.AskRes;
 import com.synergy.backend.domain.ask.repository.ReplyRepository;
+import com.synergy.backend.global.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.synergy.backend.domain.ask.model.entity.QReply.reply;
 
 @Service
 @RequiredArgsConstructor
@@ -29,9 +36,9 @@ public class AskService {
     private final ReplyRepository replyRepository;
 
     //문의 생성
-    public AskRes createAsks(AskReq askReq) {
+    public AskRes createAsks(AskReq askReq, Long idx) throws BaseException {
         // 회원 조회
-        Member member = memberRepository.findById(askReq.getMemberIdx())
+        Member member = memberRepository.findById(idx)
                 .orElseThrow(() -> new IllegalArgumentException("해당 회원을 찾을 수 없습니다."));
         // 상품 조회
         Product product = productRepository.findById(askReq.getProductIdx())
@@ -48,28 +55,28 @@ public class AskService {
         askRepository.save(ask);
 
         // DTO로 변환하여 BaseResponse로 반환
-        AskRes askRes = new AskRes(
-                ask.getMember().getIdx(),
-                ask.getMember().getNickname(),
-                ask.getMember().getProfileImageUrl(),
-                ask.getContent(),
-                ask.getIdx(),
-                ask.isSecret(),
-                null // 생성 시에는 Reply가 아직 없음
-        );
-
+        AskRes askRes = AskRes.builder()
+                .memberIdx(ask.getMember().getIdx())
+                .username(ask.getMember().getNickname())
+                .profileImageUrl(ask.getMember().getProfileImageUrl())
+                .content(ask.getContent())
+                .isSecret(ask.isSecret())
+                .reply(null)  // 답변 추가
+                .build();
         return askRes;  // 성공 시 응답
     }
 
 
     // 해당 상품 문의 목록 조회 서비스 메서드
-    public List<AskRes> getAskListByProduct(Long productIdx) throws BaseException{
-        //상품조회
+    public List<AskRes> getAskListByProduct(Long productIdx, Integer page, Integer size) throws BaseException{
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "idx"));
+
+        //해당상품이 있는지 없는지
         Product product = productRepository.findById(productIdx).orElseThrow(() ->
                 new BaseException(BaseResponseStatus.NOT_FOUND_PRODUCT));
 
         //askrepository에서 해당 productid로 목록 가져오기
-        List<Ask> getAskList = askRepository.findByProductIdx(productIdx);
+        List<Ask> getAskList = askRepository.findByProductIdx(productIdx, pageable);
 
         //결과담을곳
         List<AskRes> result = new ArrayList<>();
@@ -93,8 +100,8 @@ public class AskService {
 
             AskRes askRes = AskRes.builder()
                     .memberIdx(ask.getMember().getIdx())
-                    .username(ask.getUsername())
-                    .profileImageUrl(ask.getProfileImageUrl())
+                    .username(ask.getMember().getNickname())
+                    .profileImageUrl(ask.getMember().getProfileImageUrl())
                     .content(ask.getContent())
                     .isSecret(ask.isSecret())
                     .reply(replyRes)  // 답변 추가
