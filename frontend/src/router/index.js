59ddb1052940958286c3e98f9310720360b309f2
choijw1116 <choijw1116@gmail.com>
2024-09-14 10:00:58 +0900

    [Add] add route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import App from "@/App";
-import ProductListComponent from "@/components/product/ProductListComponent"
+import ProductListComponent from "@/components/product/ProductListComponent.vue"
 import LoginPage from "@/pages/member/LoginPage.vue"
 import SignupPage from "@/pages/member/SingupPage.vue"
 import DeliveryComponent from '@/components/member/DeliveryComponent.vue';
-import Carousel from '@/components/product/ProductCarouselComponent';
+import Carousel from '@/components/product/CarouselProductListComponent.vue';
 import AskCommentComponent from "@/components/AskCommentComponent.vue";
 
 const router = createRouter({
@@ -12,15 +12,14 @@ const router = createRouter({
     routes: [
         { path: "/product-list", component: ProductListComponent },
         { path: "/", component: App },
-        { path: "/product", component: ProductListComponent },
         { path: '/deliveryAddress', component: DeliveryComponent },
         { path: '/carousel', component: Carousel },
         { path: '/ask', component: AskCommentComponent },
         { path: "/login", component: LoginPage },
         { path: "/signup", component: SignupPage },
         { path: '/deliveryAddress', component: DeliveryComponent },
-        { path: '/carousel', component: Carousel },
         { path: '/ask', component: AskCommentComponent },
+        { path: '/carousel', component: Carousel },
     ],
 });
 
