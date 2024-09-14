7a71fc3d2c8a45b19a427bef2080b465250f3fc9
choijw1116 <choijw1116@gmail.com>
2024-09-14 21:28:08 +0900

    Revert "Merge pull request #79 from beyond-sw-camp/frontend/feature/product-list"
    This reverts commit 5ec8f42eab5bfbfeb5ef6aede9771501f2fe085e, reversing
    changes made to 36000691dccad2c8f66a58117ed584cc7b1075ac.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,15 @@
 <template>
-  <div>
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <router-view></router-view>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld
+    HelloWorld
   }
 }
 </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,4 @@
-import { createApp } from 'vue';
-import App from './App.vue';
+import { createApp } from 'vue'
+import App from './App.vue'
 
-import { createPinia } from 'pinia';
-import piniaPersistedState from 'pinia-plugin-persistedstate'
-
-import router from './router';
-
-const pinia = createPinia();
-pinia.use(piniaPersistedState);
-
-const app = createApp(App);
-
-app.use(pinia);
-app.use(router);
-app.mount('#app')
+createApp(App).mount('#app')
