3956770213877ada57fa088d53eb360903932432
choijw1116 <choijw1116@gmail.com>
2024-09-14 10:29:07 +0900

    [Feat] 문의생성,조회기능 수정

--- backend/src/main/java/com/synergy/backend/domain/ask/controller/AskController.java
+++ backend/src/main/java/com/synergy/backend/domain/ask/controller/AskController.java
@@ -5,7 +5,9 @@ import com.synergy.backend.domain.ask.model.response.AskRes;
 import com.synergy.backend.domain.ask.service.AskService;
 import com.synergy.backend.global.common.BaseResponse;
 import com.synergy.backend.global.exception.BaseException;
+import com.synergy.backend.global.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,15 +20,20 @@ public class AskController {
 
     // 문의 생성 API
     @PostMapping("/create")
-    public BaseResponse<AskRes> createAsk(@RequestBody AskReq askReq) {
-        AskRes response = askService.createAsks(askReq);
+
+    public BaseResponse<AskRes> createAsk(@RequestBody AskReq askReq, @AuthenticationPrincipal CustomUserDetails customUserDetails) throws BaseException{
+        System.out.println(askReq.getProductIdx());
+        System.out.println(askReq.isSecret());
+        System.out.println(askReq.getContent());
+
+        AskRes response = askService.createAsks(askReq,customUserDetails.getIdx());
         return new BaseResponse<>(response);  // BaseResponse만 반환
     }
 
     // 문의 조회 API
-    @GetMapping("/list")
-    public BaseResponse<List<AskRes>> getAsksList(@RequestParam Long productIdx) throws BaseException {
-        List<AskRes> askResList = askService.getAskListByProduct(productIdx);
+    @GetMapping("/list/read")
+    public BaseResponse<List<AskRes>> getAsksList(@RequestParam Long productIdx,Integer page, Integer size) throws BaseException {
+        List<AskRes> askResList = askService.getAskListByProduct(productIdx, page, size);
         return new BaseResponse<>(askResList);  // BaseResponse만 반환
     }
 }
