f79b661e82c502cb392d31a9bbd85fc178a6f41f
shinebyul <qufdl8382@gmail.com>
2024-09-17 03:14:32 +0900

    [Feat]카테고리 상품 리스트 페이지 구현

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import {createRouter, createWebHistory} from "vue-router";
 import App from "@/App";
-import ProductListComponent from "@/components/product/ProductListComponent"
 import LoginPage from "@/pages/member/LoginPage.vue"
 import SignupPage from "@/pages/member/SingupPage.vue"
 import DeliveryComponent from '@/components/member/DeliveryComponent.vue';
@@ -9,21 +8,22 @@ import MemberInfoPage from "@/pages/member/MemberInfoPage.vue";
 import UpdateMemberInfoComponent from "@/components/member/UpdateMemberInfoComponent.vue"
 import CartComponent from '@/components/order/CartComponent.vue';
 import OrderListComponent from '@/components/order/OrderListComponent'
+import CategoryProduct from '@/pages/product/CategoryProductListPage.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: "/product-list", component: ProductListComponent },
         { path: "/", component: App },
         { path: "/login", component : LoginPage},
         { path: "/signup", component: SignupPage},
         { path: '/deliveryAddress', component: DeliveryComponent },
         { path: '/carousel', component: Carousel },
         { path: '/cart', component: CartComponent },
-        { path: '/order-list', component: OrderListComponent }
+        { path: '/order-list', component: OrderListComponent },
         { path: '/member/info', component : MemberInfoPage},
         { path: '/update/member/info', component: UpdateMemberInfoComponent},
-        { path: '/cart', component: CartComponent }
+        { path: '/cart', component: CartComponent },
+        { path: '/category-product', component: CategoryProduct },
     ],
 });
 
