f17b241d620ba0bddacf1aa6f90cd74631669d30
706com <ehdgusjdbswl@gmail.com>
2024-09-16 16:19:15 +0900

    [Feat] 회원 관련 라우터 설정 (#154)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,24 +6,24 @@ import SignupPage from "@/pages/member/SingupPage.vue"
 import DeliveryComponent from '@/components/member/DeliveryComponent.vue';
 import Carousel from '@/components/product/CarouselComponent';
 import MemberInfoPage from "@/pages/member/MemberInfoPage.vue";
-import UpdateMemberInfoComponent from "@/components/member/UpdateMemberInfoComponent.vue"
 import CartComponent from '@/components/order/CartComponent.vue';
 import OrderListComponent from '@/components/order/OrderListComponent'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: "/product-list", component: ProductListComponent },
         { path: "/", component: App },
-        { path: "/login", component : LoginPage},
-        { path: "/signup", component: SignupPage},
+        { path: "/product-list", component: ProductListComponent },
         { path: '/deliveryAddress', component: DeliveryComponent },
         { path: '/carousel', component: Carousel },
         { path: '/cart', component: CartComponent },
-        { path: '/order-list', component: OrderListComponent }
-        { path: '/member/info', component : MemberInfoPage},
-        { path: '/update/member/info', component: UpdateMemberInfoComponent},
-        { path: '/cart', component: CartComponent }
+        { path: '/order-list', component: OrderListComponent },
+        { path: '/cart', component: CartComponent },
+
+        //member
+        { path: "/login", component : LoginPage},   // 로그인 페이지
+        { path: "/signup", component: SignupPage}, // 회원가입 페이지
+        { path: '/member/info', component : MemberInfoPage},    //회원 수정 페이지
     ],
 });
 
