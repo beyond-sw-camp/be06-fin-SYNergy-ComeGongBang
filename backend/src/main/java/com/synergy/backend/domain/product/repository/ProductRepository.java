0a5c76a01056418a970afe484adae355f4b25305
706com <ehdgusjdbswl@gmail.com>
2024-09-18 05:13:06 +0900

    [Feat] 상품-상품이미지 Fetch Join을 통한 조회성능 개선 (#14)

--- backend/src/main/java/com/synergy/backend/domain/product/repository/ProductRepository.java
+++ backend/src/main/java/com/synergy/backend/domain/product/repository/ProductRepository.java
@@ -2,7 +2,14 @@ package com.synergy.backend.domain.product.repository;
 
 import com.synergy.backend.domain.product.model.entity.Product;
 import com.synergy.backend.domain.product.querydsl.ProductRepositoryCustom;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
+
+    @Query("SELECT p FROM Product p LEFT JOIN FETCH p.productImages WHERE p.idx = :productId")
+    Optional<Product> findByIdWithImages(@Param("productId") Long productId);
 }
