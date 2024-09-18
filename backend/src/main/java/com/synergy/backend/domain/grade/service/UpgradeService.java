bd91692c98faae20b9f0d1d6b7bf7ed413d06f32
postrel63 <postrel63@naver.com>
2024-09-18 23:53:00 +0900

    [Feat] 기능별로 서비스 클래스 작성 #170
    FetchGradeService
    GradeCalculationService
    UpgradeService

--- backend/src/main/java/com/synergy/backend/domain/grade/service/UpgradeService.java
+++ backend/src/main/java/com/synergy/backend/domain/grade/service/UpgradeService.java
@@ -1,11 +1,20 @@
 package com.synergy.backend.domain.grade.service;
 
+import com.synergy.backend.domain.grade.model.entity.Grade;
 import com.synergy.backend.domain.member.model.entity.Member;
+import com.synergy.backend.domain.member.repository.MemberRepository;
+import com.synergy.backend.global.exception.BaseException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UpgradeService {
-    public void upgrade(Member member) {
 
+    private final MemberRepository memberRepository;
+
+    public void changeGrade(Member member, Grade grade) throws BaseException {
+        member.changeGrade(grade);
+        memberRepository.save(member);
     }
 }
