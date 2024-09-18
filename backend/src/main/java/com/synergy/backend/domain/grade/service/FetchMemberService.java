b1ed6470e3f05390812aeac67d47701f357ba889
postrel63 <postrel63@naver.com>
2024-09-18 23:52:22 +0900

    [Feat] 회원 조회 인터페이스와 구현체 #170
    회원 조회는 인터페이스, 구현체로 구분해서 작성
    시나리오
    1. 전체 회원 조회
    2. 최근 1달 구매이력 존재 + 최하 등급이 아니면서 구매이력이 없는 사람

--- backend/src/main/java/com/synergy/backend/domain/grade/service/FetchMemberService.java
+++ backend/src/main/java/com/synergy/backend/domain/grade/service/FetchMemberService.java
@@ -1,22 +1,11 @@
 package com.synergy.backend.domain.grade.service;
 
-import com.synergy.backend.domain.grade.repository.GradeRepository;
 import com.synergy.backend.domain.member.model.entity.Member;
-import com.synergy.backend.domain.member.repository.MemberRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
-public class FetchMemberService {
-
-    private final MemberRepository memberRepository;
-
-    public List<Member> FetchMember() {
-
-
-        return null;
-    }
+public interface FetchMemberService {
+    public List<Member> FetchMember();
 }
