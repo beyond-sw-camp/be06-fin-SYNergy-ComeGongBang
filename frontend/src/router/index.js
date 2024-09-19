77379d2e78546727b4d7de02859938decacbef52
shinebyul <qufdl8382@gmail.com>
2024-09-20 00:44:17 +0900

    [HotFix]인덱스 에러 수정

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import App from "@/App";
 import LoginPage from "@/pages/member/LoginPage.vue"
 import SignupPage from "@/pages/member/SingupPage.vue"
 import DeliveryComponent from '@/components/member/DeliveryComponent.vue';
