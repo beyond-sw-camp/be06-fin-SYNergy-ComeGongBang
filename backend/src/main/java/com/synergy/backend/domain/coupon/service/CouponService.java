1e7230aea0db0994bfdecd69d5bb67cc250f63bc
postrel63 <postrel63@naver.com>
2024-09-19 18:39:29 +0900

    [Feat] 스케줄러에 필요한 서비스 코드와 인터페이스  #44

---
+++ backend/src/main/java/com/synergy/backend/domain/coupon/service/CouponService.java
@@ -0,0 +1,11 @@
+package com.synergy.backend.domain.coupon.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CouponService {
+
+
+}
