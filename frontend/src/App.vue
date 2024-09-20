aad1a3d8a0433dec389baeef2616d7ca4b36f9ac
shinebyul <qufdl8382@gmail.com>
2024-09-20 17:03:17 +0900

    [Feat]헤더 상단 고정으로 수정

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <HeaderComponent />
+    <!-- <HeaderComponent /> -->
+    <TopHeaderComponent/>
+    <MiddleHeaderComponent/>
+    <BottomHeaderComponent/>
     <router-view></router-view>
     <FooterComponent />
   </div>
@@ -8,13 +11,19 @@
 
 <script>
 import FooterComponent from "./components/common/FooterComponent.vue";
-import HeaderComponent from "./components/common/HeaderComponent.vue";
+// import HeaderComponent from "./components/common/HeaderComponent.vue";
+import TopHeaderComponent from './components/common/TopHeaderComponent.vue';
+import MiddleHeaderComponent from './components/common/MiddleHeaderComponent.vue';
+import BottomHeaderComponent from './components/common/BottomHeaderComponent.vue';
 
 export default {
   name: "App",
   components: {
     FooterComponent,
-    HeaderComponent,
+    // HeaderComponent,
+    TopHeaderComponent,
+    MiddleHeaderComponent,
+    BottomHeaderComponent
   },
 };
 </script>
