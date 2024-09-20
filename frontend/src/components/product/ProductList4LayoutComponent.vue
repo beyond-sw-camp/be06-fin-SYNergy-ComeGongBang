b77b21476471e2237f3a8f1dd2b8c9b979cb1acb
shinebyul <qufdl8382@gmail.com>
2024-09-20 15:44:39 +0900

    [Feat]작가 페이지 수정(#160)

--- frontend/src/components/product/ProductList4LayoutComponent.vue
+++ frontend/src/components/product/ProductList4LayoutComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="product-list-container">
-            <ProductComponent v-for="product in productStore.productList" :key="product.idx" :product="product"/>
+            <ProductComponent v-for="product in productList" :key="product.idx" :product="product"/>
         </div>
          <ObserverComponent @show="infiniteHandler"></ObserverComponent>
     </div>
@@ -22,6 +22,12 @@ export default {
         return{
             page:0,
             loading: false //로딩 관리
+        }
+    },
+    props:{
+        productList : {
+            type: Object,
+            required: true
         }
     },
     computed:{
