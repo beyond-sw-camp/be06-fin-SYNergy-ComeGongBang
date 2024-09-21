faf3c632a293f51ce4188fd667be50a0acfa070f
choijw1116 <choijw1116@gmail.com>
2024-09-22 00:22:36 +0900

    [Feat]store 소분류 카테고리 기능추가

--- frontend/src/stores/useCategoryStore.js
+++ frontend/src/stores/useCategoryStore.js
@@ -3,30 +3,65 @@ import axios from 'axios';
 
 export const useCategoryStore = defineStore('category', {
     state: () => ({
-        parentCategory: [],  //부모
-        childrenCategory: [], //자식
+        topCategoriesList: [],       // 대분류 리스트
+        middleCategoriesList: [],    // 중분류 리스트
+        bottomCategoriesList: [],    // 소분류 리스트
+        selectedTopCategory: null,    // 선택된 대분류
+        selectedMiddleCategory: null, // 선택된 중분류
+        selectedBottomCategory: null, // 선택된 소분류
         parentIdx: 0,
     }),
     actions: {
-        async categoryList() {
+        // 대분류 불러오기
+        async loadTopCategories() {
             try {
-                let url = '/api/categories/parent';
+                let url = '/api/categories/top';
                 let response = await axios.get(url);
-                this.parentCategory = response.data.result;
-                console.log("store-parent", response)
+                this.topCategoriesList = response.data.result;  // 상태 업데이트
+                console.log("store-top", response.data.result);
             } catch (error) {
-                console.log(error)
+                console.log(error);
             }
         },
-        async childrenCategoryList(parentIdx) {
+
+        // 중분류 불러오기
+        async loadMiddleCategories(topIdx) {
             try {
-                let url = `/api/categories/child/${parentIdx}`;
+                let url = `/api/categories/middle/${topIdx}`;
                 let response = await axios.get(url);
-                this.childrenCategory = response.data.result;
-                console.log("store-child", response)
+                this.middleCategoriesList = response.data.result;  // 상태 업데이트
+                this.selectedTopCategory = topIdx;  // 대분류 선택 시 상태 저장
+                console.log("store-middle", response.data.result);
             } catch (error) {
-                console.log(error)
+                console.log(error);
             }
+        },
+
+        // 소분류 불러오기
+        async loadBottomCategories(middleIdx) {
+            try {
+                let url = `/api/categories/bottom/${middleIdx}`;
+                let response = await axios.get(url);
+                this.bottomCategoriesList = response.data.result;  // 상태 업데이트
+                this.selectedMiddleCategory = middleIdx;  // 중분류 선택 시 상태 저장
+                console.log("store-bottom", response.data.result);
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
+        // 대분류 선택
+        selectTopCategory(categoryIdx) {
+            this.selectedTopCategory = categoryIdx;
+            this.loadMiddleCategories(categoryIdx);  // 중분류 불러오기
+            console.log("store-middleCategories", this.selectedTopCategory);
+        },
+
+        // 중분류 선택
+        selectMiddleCategory(categoryIdx) {
+            this.selectedMiddleCategory = categoryIdx;
+            this.loadBottomCategories(categoryIdx);  // 소분류 불러오기
+            console.log("store-bottomCategories", this.selectedMiddleCategory);
         },
     },
 });
