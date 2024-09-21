2d336e41ea11846de090ce11fa928a811d4e9d0f
postrel63 <postrel63@naver.com>
2024-09-21 23:05:32 +0900

    [Feat] SignupPage 모듈이 안읽혀서 다시 주입
    저장하면 자동정렬되서 들여쓰기랑 띄여쓰기가 일부 계속 변경됩니다.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,22 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
-import LoginPage from '@/pages/member/LoginPage.vue';
-import SignupPage from '@/pages/member/SingupPage.vue';
-import DeliveryComponent from '@/components/member/DeliveryComponent.vue';
+import LoginPage from "@/pages/member/LoginPage.vue";
+import SignupPage from "@/pages/member/SignupPage.vue";
+import DeliveryComponent from "@/components/member/DeliveryComponent.vue";
 import MemberInfoPage from "@/pages/member/MemberInfoPage.vue";
-import CartComponent from '@/components/order/CartComponent.vue';
+import CartComponent from "@/components/order/CartComponent.vue";
 import MyFavoriteListComponent from "@/components/mypage/MyFavoriteListComponent.vue";
 import MyPage from "@/pages/mypage/MyPage.vue";
-import MainPage from '@/pages/MainPage'
-import OrderPayment from '@/pages/payment/OrderPaymentPage';
-import PresentPayment from '@/pages/payment/PresentPaymentPage';
-import AtelierPage from '@/pages/atelier/AtelierPage';
-import AtelierProducts from '@/components/atelier/AtelierProductListComponent';
-import AtelierProfile from '@/components/atelier/AtelierProfileComponent';
+import MainPage from "@/pages/MainPage";
+import OrderPayment from "@/pages/payment/OrderPaymentPage";
+import PresentPayment from "@/pages/payment/PresentPaymentPage";
+import AtelierPage from "@/pages/atelier/AtelierPage";
+import AtelierProducts from "@/components/atelier/AtelierProductListComponent";
+import AtelierProfile from "@/components/atelier/AtelierProfileComponent";
 import AskCommentComponent from "@/components/AskCommentComponent.vue";
 import EmailFindPage from "@/pages/member/MemberEmailFindPage";
-import GradeComponent from '@/components/mypage/GradeComponent.vue';
+import GradeComponent from "@/components/mypage/GradeComponent.vue";
 import LoginCallBackComponent from "@/components/member/LoginCallBackComponent";
-import { useMemberStore } from '@/stores/useMemberStore';
+import { useMemberStore } from "@/stores/useMemberStore";
 
 const requireLogin = async (to, from, next) => {
   const memberStore = useMemberStore();
@@ -28,7 +28,7 @@ const requireLogin = async (to, from, next) => {
   }
 
   next({
-    path: '/login',
+    path: "/login",
     query: { redirect: to.fullPath },
   });
 };
@@ -37,88 +37,88 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: '/mypage',
+      path: "/mypage",
       component: MyPage, // 고정
       children: [
         {
-          path: 'favorite/likes',
-          name: 'likes',
+          path: "favorite/likes",
+          name: "likes",
           component: MyFavoriteListComponent,
           props: { initialTab: 0 },
         },
         {
-          path: 'favorite/follow-artist',
-          name: 'follow-artist',
+          path: "favorite/follow-artist",
+          name: "follow-artist",
           component: MyFavoriteListComponent,
           props: { initialTab: 1 },
         },
         {
-          path: 'favorite/recent-view',
-          name: 'recent-view',
+          path: "favorite/recent-view",
+          name: "recent-view",
           component: MyFavoriteListComponent,
           props: { initialTab: 2 },
         },
         {
-          path: '/deliveryAddress',
-          name: 'deliveryAddress',
+          path: "/deliveryAddress",
+          name: "deliveryAddress",
           component: DeliveryComponent,
         },
-        { path: '/grade', name: 'grade', component: GradeComponent },
+        { path: "/grade", name: "grade", component: GradeComponent },
       ],
     },
 
-        { path: "/main", component: MainPage },
+    { path: "/main", component: MainPage },
 
     // 장바구니, 구매, 선물
     {
-      path: '/cart',
-      name: 'Cart',
+      path: "/cart",
+      name: "Cart",
       component: CartComponent,
       beforeEnter: requireLogin,
-      props: { pageType: 'cart' },
+      props: { pageType: "cart" },
     },
     {
-      path: '/cart/direct/:encryptedCartIdx',
-      name: 'OrderPage',
+      path: "/cart/direct/:encryptedCartIdx",
+      name: "OrderPage",
       component: CartComponent,
       beforeEnter: requireLogin,
       props: (route) => ({
-        pageType: 'order',
+        pageType: "order",
         encryptedCartIdx: route.params.encryptedCartIdx,
       }),
     },
     {
-      path: '/cart/gift/:encryptedCartIdx',
-      name: 'GiftPage',
+      path: "/cart/gift/:encryptedCartIdx",
+      name: "GiftPage",
       component: CartComponent,
       beforeEnter: requireLogin,
       props: (route) => ({
-        pageType: 'gift',
+        pageType: "gift",
         encryptedCartIdx: route.params.encryptedCartIdx,
       }),
     },
 
-    { path: '/order/payment', component: OrderPayment },
-    { path: '/present/payment', component: PresentPayment },
+    { path: "/order/payment", component: OrderPayment },
+    { path: "/present/payment", component: PresentPayment },
 
-        {
-            path: '/atelier', component: AtelierPage,
-            children: [
-                { path: '', redirect: '/profile' },
-                { path: '/products', component: AtelierProducts },
-                { path: '/profile', component: AtelierProfile },
-            ]
-        },
-        { path: '/ask', component: AskCommentComponent },
+    {
+      path: "/atelier",
+      component: AtelierPage,
+      children: [
+        { path: "", redirect: "/profile" },
+        { path: "/products", component: AtelierProducts },
+        { path: "/profile", component: AtelierProfile },
+      ],
+    },
+    { path: "/ask", component: AskCommentComponent },
 
-        //member
-        { path: "/login", component: LoginPage },   // 로그인 페이지
-        { path: "/login-callback", component: LoginCallBackComponent },   // 소셜 로그인 콜백
-        { path: "/signup", component: SignupPage }, // 회원가입 페이지
-        { path: '/member/info', component: MemberInfoPage },    //회원 수정 페이지
-        { path: '/member/find', component: EmailFindPage },    //회원 찾기 페이지
-
-    ],
+    //member
+    { path: "/login", component: LoginPage }, // 로그인 페이지
+    { path: "/login-callback", component: LoginCallBackComponent }, // 소셜 로그인 콜백
+    { path: "/signup", component: SignupPage }, // 회원가입 페이지
+    { path: "/member/info", component: MemberInfoPage }, //회원 수정 페이지
+    { path: "/member/find", component: EmailFindPage }, //회원 찾기 페이지
+  ],
 });
 
 export default router;
