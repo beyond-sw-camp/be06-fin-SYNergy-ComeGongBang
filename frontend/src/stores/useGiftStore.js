db68021ecfe8fef04af91f0f6b67dc931bee9416
shinebyul <qufdl8382@gmail.com>
2024-09-21 19:15:23 +0900

    [Feat]내가 받은 선물 상세보기 수정(#112)

--- frontend/src/stores/useGiftStore.js
+++ frontend/src/stores/useGiftStore.js
@@ -78,7 +78,6 @@ export const useGiftStore = defineStore("gift", {
             count : 2,
             date : "2024.09.12",
             fromUser : "요아정완",
-            fromPhone : "010-7280-0916",
             message : "for you~~~",
             price : 12000,
             discount : 300,
@@ -86,12 +85,22 @@ export const useGiftStore = defineStore("gift", {
             deliveryFee : 2500,
             totalPrice : 14100,
             payment : "카카오페이",
+            status : 1, //선물받기 완료된 상태
+            deliveryStatus : "작가 발송 완료",
+            deliveryInfo:{
+                toUser : "한별",
+                phone : "01072800916",
+                post : 1234,
+                address : "서울시 관악구 어쩌고",
+                addressDetail : "4층"
+            },
             products:[
                 {
                     idx : 1,
                     state : "취소 완료",
                     name : "도라지 정과",
                     atelier : "한길로 그대로",
+                    count : 1,
                     image_url : "https://springprac2024-s3.s3.ap-northeast-2.amazonaws.com/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA+2024-09-12+%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE+4.11.19.png"
                 },
                 {
@@ -99,6 +108,7 @@ export const useGiftStore = defineStore("gift", {
                     state : "취소 완료",
                     name : "머랭쿠키",
                     atelier : "짱맛있어",
+                    count : 1,
                     image_url : "https://springprac2024-s3.s3.ap-northeast-2.amazonaws.com/%E1%84%82%E1%85%A9%E1%84%82%E1%85%A9.png"
                 }
             ]
