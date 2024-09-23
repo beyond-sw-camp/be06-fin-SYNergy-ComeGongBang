53c41bfeb441bcae0e9d108a63b4f15b07e37090
postrel63 <postrel63@naver.com>
2024-09-23 18:21:41 +0900

    [Feat] 페이징 처리는 Query를 직접 사용, 리뷰에 사용된 사진은 한개만 #18

--- backend/src/main/java/com/synergy/backend/domain/review/service/ReviewService.java
+++ backend/src/main/java/com/synergy/backend/domain/review/service/ReviewService.java
@@ -6,17 +6,17 @@ import com.synergy.backend.domain.product.model.entity.Product;
 import com.synergy.backend.domain.product.repository.ProductRepository;
 import com.synergy.backend.domain.review.model.entity.Review;
 import com.synergy.backend.domain.review.model.request.CreateReviewReq;
+import com.synergy.backend.domain.review.model.response.MyReviewListRes;
+import com.synergy.backend.domain.review.model.response.ProductReviewRes;
 import com.synergy.backend.domain.review.model.response.ReadDetailReviewRes;
-import com.synergy.backend.domain.review.model.response.ReadListReviewRes;
 import com.synergy.backend.domain.review.repository.ReviewRepository;
 import com.synergy.backend.global.common.BaseResponseStatus;
 import com.synergy.backend.global.exception.BaseException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -26,31 +26,28 @@ public class ReviewService {
     private final ProductRepository productRepository;
 
     // 리뷰 작성
-    public void createReview(CreateReviewReq req) throws BaseException {
-        Optional<Member> member = memberRepository.findById(req.getMemberIdx());
-        Optional<Product> product = productRepository.findById(req.getProductIdx());
-        Review saved = reviewRepository.save(req.toEntity(
-                member.orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_USER)),
-                product.orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_PRODUCT))));
+    public void createReview(CreateReviewReq req, Long memberIdx) throws BaseException {
+        Member member = memberRepository.findById(memberIdx).orElseThrow(() ->
+                new BaseException(BaseResponseStatus.NOT_FOUND_USER));
+
+        Product product = productRepository.findById(req.getProductIdx()).orElseThrow(() ->
+                new BaseException(BaseResponseStatus.NOT_FOUND_PRODUCT));
+
+        if (req.getContent().length() < 15) {
+            throw new BaseException(BaseResponseStatus.MIN_REVIEW_LENGTH);
+        }
+
+        if (req.getScore() < 0 || req.getScore() > 5) {
+            throw new BaseException(BaseResponseStatus.INVALID_RATING);
+        }
+
+        reviewRepository.save(req.toEntity(member, product));
     }
 
     // 해당 상품 리뷰 리스트 조회
-    public List<ReadListReviewRes> readReviewList(Long productIdx) throws BaseException {
-        List<Review> allByProductIdx = reviewRepository.findByProductIdx(productIdx);
-        List<ReadListReviewRes> result = new ArrayList<>();
-
-        for (Review review : allByProductIdx) {
-            result.add(
-                    ReadListReviewRes.builder()
-                            .idx(review.getIdx())
-                            .nickname(review.getMember().getNickname())
-                            .profileImageUrl(review.getMember().getProfileImageUrl())
-                            .createdAt(review.getCreatedAt())
-                            .content(review.getContent())
-                            .score(review.getScore())
-                            .build());
-        }
-        return result;
+    public Page<ProductReviewRes> readReviewList(Long productIdx, int page, int size) throws BaseException {
+        Pageable pageable = PageRequest.of(page, size);
+        return reviewRepository.findByProductIdx(productIdx, pageable);
     }
 
     // 리뷰 상세 조회
@@ -58,18 +55,13 @@ public class ReviewService {
         Review review = reviewRepository.findById(reviewIdx)
                 .orElseThrow(() -> new BaseException(BaseResponseStatus.NOT_FOUND_REVIEW));
 
-        ReadDetailReviewRes result = ReadDetailReviewRes.builder()
-                .reviewIdx(reviewIdx)
-                .nickname(review.getMember().getNickname())
-                .profileImageUrl(review.getMember().getProfileImageUrl())
-                .createdAt(review.getCreatedAt())
-                .productName(review.getProduct().getName())
-                .thumbnailUrl(review.getProduct().getThumbnailUrl())
-                .content(review.getContent())
-                .score(review.getScore())
-                .build();
-
-        return result;
+        return ReadDetailReviewRes.from(reviewIdx, review);
     }
 
+
+    public Page<MyReviewListRes> getMyReviewList(Long memberIdx, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return reviewRepository.findByReVIewByMemberIdx(memberIdx, pageable);
+
+    }
 }
