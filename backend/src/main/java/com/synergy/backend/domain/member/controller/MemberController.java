f6abc4d5401ebbdce8040148851a16549159adfc
postrel63 <postrel63@naver.com>
2024-09-24 17:16:07 +0900

    0515 테스트 01

--- backend/src/main/java/com/synergy/backend/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/synergy/backend/domain/member/controller/MemberController.java
@@ -1,4 +1,16 @@
 package com.synergy.backend.domain.member.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
 public class MemberController {
+
+    @GetMapping
+    public String test(){
+        return "test01";
+    }
+
 }
