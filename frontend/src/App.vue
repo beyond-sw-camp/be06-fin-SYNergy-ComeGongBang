d730d2028e86401bae12dc91505ec03750cd996b
706com <ehdgusjdbswl@gmail.com>
2024-09-24 20:41:02 +0900

    [Test] jenkins 테스트 6

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>hihihihihi</h1>
+    <h1>hihihihihihi</h1>
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <router-view></router-view>
