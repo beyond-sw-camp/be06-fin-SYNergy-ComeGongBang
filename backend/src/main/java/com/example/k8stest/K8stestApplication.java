a3cc714b0ff989e9bf79098705687d80c3687cba
706com <ehdgusjdbswl@gmail.com>
2024-09-27 17:48:25 +0900

    [Test] DB 테스트1

---
+++ backend/src/main/java/com/example/k8stest/K8stestApplication.java
@@ -0,0 +1,13 @@
+package com.example.k8stest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8stestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8stestApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/k8stest/K8stestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.k8stest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8stestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
