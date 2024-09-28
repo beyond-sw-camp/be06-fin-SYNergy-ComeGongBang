b2391e9878a9ff27b638c473d6f4849416e30bd3
706com <ehdgusjdbswl@gmail.com>
2024-09-28 17:26:25 +0900

    [Feat] 공방 상품에 대한 좋아요여부 기능 구현 (성능 Low.ver)

--- backend/src/main/java/com/synergy/backend/domain/atelier/service/AtelierService.java
+++ backend/src/main/java/com/synergy/backend/domain/atelier/service/AtelierService.java
@@ -3,8 +3,8 @@ package com.synergy.backend.domain.atelier.service;
 import com.synergy.backend.domain.atelier.model.entity.Atelier;
 import com.synergy.backend.domain.atelier.model.response.AtelierProfileInfoRes;
 import com.synergy.backend.domain.atelier.repository.AtelierRepository;
-import com.synergy.backend.domain.follow.repository.FollowRepository;
 import com.synergy.backend.domain.follow.service.FollowService;
+import com.synergy.backend.domain.likes.repository.LikesRepository;
 import com.synergy.backend.domain.member.model.entity.Member;
 import com.synergy.backend.domain.member.repository.MemberRepository;
 import com.synergy.backend.domain.product.model.entity.Product;
@@ -23,27 +23,43 @@ public class AtelierService {
     private final AtelierRepository atelierRepository;
     private final ProductRepository productRepository;
     private final MemberRepository memberRepository;
+    private final LikesRepository likesRepository;
     private final FollowService followService;
 
+    // 특정 공방 상품 전체 조회기능 + 좋아요 추가
     public List<ProductListRes> atelierProductList(Long atelierIdx, Long memberIdx) throws BaseException {
+
+        Member member = null;
+        if (memberIdx != null) {
+            member = memberRepository.findById(memberIdx).get();
+        }
+
+        // 1.공방 정보를 꺼내오고, 해당 공방의 상품정보를 꺼내온다.
         Atelier atelier = atelierRepository.findById(atelierIdx).orElseThrow(
                 () -> new BaseException(BaseResponseStatus.NOT_FOUND_ATELIER)
         );
-
-        List<Product> results = productRepository.findAllByAtelier(atelier);
+        List<Product> productsMadeByAtelier = productRepository.findAllByAtelier(atelier);
 
         List<ProductListRes> atelierProducts = new ArrayList<>();
-        for (Product result : results) {
-            atelierProducts.add(ProductListRes.builder()
-                    .idx(result.getIdx())
-                    .name(result.getName())
-                    .price(result.getPrice())
-                    .thumbnailUrl(result.getThumbnailUrl())
-                    .averageScore(result.getAverageScore())
+        boolean isMemberLiked = false;
+        for (Product p : productsMadeByAtelier) {
+            // 2. 각 상품에 대해서 회원이 좋아요를 했는지 찾는다.
+            if (member != null) {
+                isMemberLiked = likesRepository.existsByMemberAndProduct(member, p);
+            }
+
+            // 3. 각 상품과 해당 상품을 회원이 좋아요 했는지를 상품정보에 담아서 보낸다.
+            ProductListRes product = ProductListRes.builder()
+                    .idx(p.getIdx())
+                    .name(p.getName())
+                    .price(p.getPrice())
+                    .thumbnailUrl(p.getThumbnailUrl())
+                    .averageScore(p.getAverageScore())
                     .atelier_name(atelier.getName())
-                    .category_name(result.getCategory().getCategoryName())
-//                   //TODO .isMemberliked(result.getIsMemberliked())
-                    .build());
+                    .category_name(p.getCategory().getCategoryName())
+                    .isMemberliked(isMemberLiked)
+                    .build();
+            atelierProducts.add(product);
         }
 
         return atelierProducts;
