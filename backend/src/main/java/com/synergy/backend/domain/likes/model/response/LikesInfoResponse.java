7444bf073220ecb82d4201e687a7e8fe61cd410c
706com <ehdgusjdbswl@gmail.com>
2024-09-30 00:16:36 +0900

    [Hotfix] Response 객체 인식 수정

--- backend/src/main/java/com/synergy/backend/domain/likes/model/response/LikesInfoResponse.java
+++ backend/src/main/java/com/synergy/backend/domain/likes/model/response/LikesInfoResponse.java
@@ -1,8 +1,10 @@
 package com.synergy.backend.domain.likes.model.response;
 
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class LikesInfoResponse {
     private boolean memberIsLiked;
     private Long productLikesCount;
