8377c29e468885d786be8493a291a74e294be0e8
706com <ehdgusjdbswl@gmail.com>
2024-09-29 19:02:21 +0900

    [Fix] 오타 수정 (#22)

--- backend/src/main/java/com/synergy/backend/domain/product/repository/ProductRepository.java
+++ backend/src/main/java/com/synergy/backend/domain/product/repository/ProductRepository.java
@@ -15,7 +15,8 @@ public interface ProductRepository extends JpaRepository<Product, Long>, Product
     @Query("SELECT p FROM Product p LEFT JOIN FETCH p.productImages WHERE p.idx = :productIdx")
     Optional<Product> findByIdWithImages(@Param("productIdx") Long productIdx);
 
-    @Query("SELECT h.`name FROM ProductHashtag ph JOIN ph.hashtag h WHERE ph.product.idx = :productIdx")
+    @Query("SELECT h.name FROM ProductHashtag ph JOIN ph.hashtag h WHERE ph.product.idx = :productIdx")
     List<String> findHashtagsByProductIdx(@Param("productIdx") Long productIdx);
 
 }
+
