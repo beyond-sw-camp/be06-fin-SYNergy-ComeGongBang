53b46953f23adb81829367ed9f7b5aca45790d43
choijw1116 <choijw1116@gmail.com>
2024-09-30 09:24:04 +0900

    [Feat] 팔로우 스토어 생성

---
+++ frontend/src/stores/useFollowStore.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { defineStore } from 'pinia';
+
+export const useFollowStore = defineStore('review', {
+  state: () => ({
+    atelierIdx: '', //공방idx
+    atelierName: '', //공방이름
+    atelierDescription: '', //공방설명
+    atelierProfileImage: '', //공방이미지
+    productImages: [], //공방상품이미지
+    followList: [], //팔로우한 작가들리스트
+  }),
+
+  actions: {
+    // 팔로우 리스트 조회
+    async fetcchFollow(page = 0, size = 10) {
+      try {
+        let url = `/api/follow`;
+        let response = await axios.get(url, { withCredentials: true },
+          { params: { page, size } }
+        )
+        this.followList = response.data.result;
+        this.page = response.data.result.page;
+        if (response.status === 200) {
+          return this.followList;
+        }
+      } catch (error) {
+        console.error('팔로우 리스트 조회 실패:', error);
+      }
+
+    },
+  },
+});
