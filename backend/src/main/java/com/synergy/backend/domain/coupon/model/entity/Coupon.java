0add690a6b2013e46a40e12dbb68752400e43c91
postrel63 <postrel63@naver.com>
2024-10-01 02:09:52 +0900

    [Feat] 이벤트 쿠폰 엔티티 설정  #53

--- backend/src/main/java/com/synergy/backend/domain/coupon/model/entity/Coupon.java
+++ backend/src/main/java/com/synergy/backend/domain/coupon/model/entity/Coupon.java
@@ -1,16 +1,16 @@
 package com.synergy.backend.domain.coupon.model.entity;
 
 import com.synergy.backend.domain.coupon.model.type.CouponType;
+import com.synergy.backend.domain.coupon.model.type.IssueDate;
 import com.synergy.backend.domain.grade.model.entity.Grade;
 import com.synergy.backend.global.common.model.BaseEntity;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
 @Table(name = "coupon")
 public class Coupon extends BaseEntity {
     @Id
@@ -24,7 +24,30 @@ public class Coupon extends BaseEntity {
     private String code;
     private Integer discountPercent;
 
+    private Integer totalQuantity;
+    private Integer issuedQuantity = 0;
+    @Embedded
+    private IssueDate issueDate;
+
+    private LocalDateTime usageTimes;
+
+
+
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "grade_idx")
+    @JoinColumn(name = "grade_idx", nullable = true)
     private Grade grade;
+
+
+    // 발급 가능,수량 제한 없을 때, 선착순 쿠폰이면 수량이 남아있을 때
+    public boolean isAvailable() {
+        return totalQuantity == null || issuedQuantity < totalQuantity;
+    }
+
+    public void increaseCouponQuantity() {
+        if (this.totalQuantity != null
+                && this.issuedQuantity < this.totalQuantity) {
+            this.issuedQuantity++;
+        }
+    }
+
 }
