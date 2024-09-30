f5c8cebe499f8fd2b0f1250399a40023635f9b33
choijw1116 <choijw1116@gmail.com>
2024-09-30 09:23:19 +0900

    [Feat] 팔로우하는 작가 컴포넌트

--- frontend/src/components/mypage/FollowAtelierListComponent.vue
+++ frontend/src/components/mypage/FollowAtelierListComponent.vue
@@ -1,277 +1,210 @@
   <template>
-  <div class="PageFavoriteListDesktop w-full pl-6">
-    <p class="flex items-center headline4-bold-small mb-[36px]">관심</p>
-    <div class="DesktopFollowArtistItem" data-v-7ab1112c="">
+  <div class="DesktopFollowArtistItem" data-v-7ab1112c="">
+    <div
+      class="flex w-[381px] py-[28px] pl-[31px] pr-[36px] cursor-pointer"
+      data-v-7ab1112c=""
+    >
       <div
-        class="flex w-[381px] py-[28px] pl-[31px] pr-[36px] cursor-pointer"
+        class="flex items-center w-[100px] relative mr-[32px]"
         data-v-7ab1112c=""
       >
         <div
-          class="flex items-center w-[100px] relative mr-[32px]"
+          class="BaseAvatarArtist"
+          style="--overlay-size: 134; --overlay-image: url()"
           data-v-7ab1112c=""
+          data-v-4bef34a0=""
         >
           <div
-            class="BaseAvatarArtist"
-            style="--overlay-size: 134; --overlay-image: url()"
-            data-v-7ab1112c=""
+            class="BaseAvatar BaseAvatar--border"
+            style="
+              --BaseAvatar-image: url(//image.idus.com/image/files/51935885594142fc86bd88c280eecb76_320.jpg);
+              --BaseAvatar-size: 96;
+            "
+            data-v-2fc5c54e=""
             data-v-4bef34a0=""
           >
+            <div class="BaseAvatar__avatar" data-v-2fc5c54e="">
+              <div class="BaseAvatar__avatarImage" data-v-2fc5c54e=""></div>
+            </div>
             <div
-              class="BaseAvatar BaseAvatar--border"
+              class="CoreBadge BaseAvatar__iconDot"
               style="
-                --BaseAvatar-image: url(//image.idus.com/image/files/51935885594142fc86bd88c280eecb76_320.jpg);
-                --BaseAvatar-size: 96;
+                --CoreBadge-color-background: #ef7014;
+                --CoreBadge-color-outline: #ffffff;
+                --CoreBadge-size: 8;
+                --CoreBadge-fontSize: 11;
+                --CoreBadge-offset-x: 0;
+                --CoreBadge-offset-y: 0;
+                --CoreBadge-position: relative;
               "
+              data-v-8f163f81=""
               data-v-2fc5c54e=""
-              data-v-4bef34a0=""
-            >
-              <div class="BaseAvatar__avatar" data-v-2fc5c54e="">
-                <div class="BaseAvatar__avatarImage" data-v-2fc5c54e=""></div>
-              </div>
-              <div
-                class="CoreBadge BaseAvatar__iconDot"
-                style="
-                  --CoreBadge-color-background: #ef7014;
-                  --CoreBadge-color-outline: #ffffff;
-                  --CoreBadge-size: 8;
-                  --CoreBadge-fontSize: 11;
-                  --CoreBadge-offset-x: 0;
-                  --CoreBadge-offset-y: 0;
-                  --CoreBadge-position: relative;
-                "
-                data-v-8f163f81=""
-                data-v-2fc5c54e=""
-              ></div>
-              <div
-                class="CoreBadge CoreBadge__position--bottom BaseAvatar__iconCamera"
-                style="
-                  --CoreBadge-color-background: #000000;
-                  --CoreBadge-color-outline: #ffffff;
-                  --CoreBadge-size: 28;
-                  --CoreBadge-fontSize: 11;
-                  --CoreBadge-offset-x: 0;
-                  --CoreBadge-offset-y: 0;
-                  --CoreBadge-position: relative;
-                "
-                data-v-8f163f81=""
-                data-v-2fc5c54e=""
-              ></div>
-            </div>
-            <div class="BaseAvatarArtist__overlay" data-v-4bef34a0=""></div>
-          </div>
-        </div>
-        <div class="min-w-[182px] flex flex-col" data-v-7ab1112c="">
-          <p
-            class="subtitle3_bold_small mb-[8px] line-clamp-1"
-            data-v-7ab1112c=""
-          >
-            춘당사
-          </p>
-          <p
-            class="body3_regular_medium line-clamp-3 mb-[24px] min-h-[54px]"
-            data-v-7ab1112c=""
-          >
-            2018년 4월20일에 매장오픈하고, 서울디저트페어(할로윈전),(딸기전)에
-            참여해 완판하고 왔습니다. 이때 아이디어스 직원분께서 저희집 디저트를
-            드시고 입점제의가 들어왔구요. 그 분의 인연으로 지금까지
-            아이디어스에서 다양한 제품을 선보이고 있답니다
-          </p>
-          <button
-            type="outline"
-            class="CoreButton BaseButtonRectangle body3-regular-small BaseButtonRectangle__outline min-w-[82px] mr-[1px]"
-            style="
-              background-color: #ffffff;
-              color: #333333;
-              height: 32px;
-              flex-direction: row;
-              --core-button-padding-x: 8;
-              --button-rectangle-border-color: #acacac;
-            "
-            data-v-524f63ea=""
-            data-v-7940d6dd=""
-            data-v-7ab1112c=""
-          >
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
+            ></div>
+            <div
+              class="CoreBadge CoreBadge__position--bottom BaseAvatar__iconCamera"
               style="
-                width: 16px;
-                height: 16px;
-                opacity: 1;
-                fill: currentColor;
-                --BaseIcon-color: #333333;
-                margin-right: 2px;
+                --CoreBadge-color-background: #000000;
+                --CoreBadge-color-outline: #ffffff;
+                --CoreBadge-size: 28;
+                --CoreBadge-fontSize: 11;
+                --CoreBadge-offset-x: 0;
+                --CoreBadge-offset-y: 0;
+                --CoreBadge-position: relative;
               "
-              class="BaseIcon CoreButton__icon"
-              data-v-6d2bd019=""
-              data-v-524f63ea=""
+              data-v-8f163f81=""
+              data-v-2fc5c54e=""
+            ></div>
+          </div>
+          <div class="BaseAvatarArtist__overlay" data-v-4bef34a0=""></div>
+        </div>
+      </div>
+      <div class="min-w-[182px] flex flex-col" data-v-7ab1112c="">
+        <p
+          class="subtitle3_bold_small mb-[8px] line-clamp-1"
+          data-v-7ab1112c=""
+        >
+          <!-- 춘당사 -->
+          {{ productList.atelierName }}
+        </p>
+        <p
+          class="body3_regular_medium line-clamp-3 mb-[24px] min-h-[54px]"
+          data-v-7ab1112c=""
+        >
+          <!-- 2018년 4월20일에 매장오픈하고, 서울디저트페어(할로윈전),(딸기전)에
+          참여해 완판하고 왔습니다. 이때 아이디어스 직원분께서 저희집 디저트를
+          드시고 입점제의가 들어왔구요. 그 분의 인연으로 지금까지 아이디어스에서
+          다양한 제품을 선보이고 있답니다 -->
+          {{ productList.atelierDescription }}
+        </p>
+        <button
+          type="outline"
+          class="CoreButton BaseButtonRectangle body3-regular-small BaseButtonRectangle__outline min-w-[82px] mr-[1px]"
+          style="
+            background-color: #ffffff;
+            color: #333333;
+            height: 32px;
+            flex-direction: row;
+            --core-button-padding-x: 8;
+            --button-rectangle-border-color: #acacac;
+          "
+          data-v-524f63ea=""
+          data-v-7940d6dd=""
+          data-v-7ab1112c=""
+          @click="clickFollowBtn(productList.atelierIdx)"
+        >
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            style="
+              width: 16px;
+              height: 16px;
+              opacity: 1;
+              fill: currentColor;
+              --BaseIcon-color: #333333;
+              margin-right: 2px;
+            "
+            class="BaseIcon CoreButton__icon"
+            data-v-6d2bd019=""
+            data-v-524f63ea=""
+          >
+            <g clip-path="url(#clip0_124_2952)">
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M19.4697 5.46973L20.5303 6.53039L9.53033 17.5304C9.26406 17.7967 8.8474 17.8209 8.55378 17.603L8.46967 17.5304L3.46967 12.5304L4.53033 11.4697L9 15.9391L19.4697 5.46973Z"
+              ></path>
+            </g>
+            <defs>
+              <clipPath id="clip0_124_2952">
+                <rect width="24" height="24"></rect>
+              </clipPath>
+            </defs>
+          </svg>
+          <div class="inline-flex items-center" data-v-524f63ea="">
+            <!--[--><!--]--><span class="CoreButton__text" data-v-524f63ea=""
+              >팔로잉</span
             >
-              <g clip-path="url(#clip0_124_2952)">
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M19.4697 5.46973L20.5303 6.53039L9.53033 17.5304C9.26406 17.7967 8.8474 17.8209 8.55378 17.603L8.46967 17.5304L3.46967 12.5304L4.53033 11.4697L9 15.9391L19.4697 5.46973Z"
-                ></path>
-              </g>
-              <defs>
-                <clipPath id="clip0_124_2952">
-                  <rect width="24" height="24"></rect>
-                </clipPath>
-              </defs>
-            </svg>
-            <div class="inline-flex items-center" data-v-524f63ea="">
-              <!--[--><!--]--><span class="CoreButton__text" data-v-524f63ea=""
-                >팔로잉</span
-              >
-            </div>
-            <!--]-->
-          </button>
-        </div>
-      </div>
-      <div class="w-[100%] flex h-[198px] cursor-pointer" data-v-7ab1112c="">
-        <!--[-->
-        <div
-          class="BaseImage flex-[1_33%]"
-          style="
-            --BaseImage-width: 0;
-            --BaseImage-height: 0;
-            --BaseImage-radius: 0;
-          "
-          data-v-24b1dfb3=""
-          data-v-7ab1112c=""
-        >
-          <div
-            class="BaseImage__image"
-            style="
-              background-image: url(//image.idus.com/image/files/c77e09f10fcd446bb1214326fc89e8a6_512.jpg);
-            "
-            data-v-24b1dfb3=""
-          >
-            <img
-              src="//image.idus.com/image/files/c77e09f10fcd446bb1214326fc89e8a6_512.jpg"
-              class="hidden"
-              loading="lazy"
-              data-v-24b1dfb3=""
-            />
           </div>
-        </div>
-        <div
-          class="BaseImage flex-[1_33%]"
-          style="
-            --BaseImage-width: 0;
-            --BaseImage-height: 0;
-            --BaseImage-radius: 0;
-          "
-          data-v-24b1dfb3=""
-          data-v-7ab1112c=""
-        >
-          <div
-            class="BaseImage__image"
-            style="
-              background-image: url(//image.idus.com/image/files/61e7b33b950a477b9723eb7411ead0d4_512.jpg);
-            "
-            data-v-24b1dfb3=""
-          >
-            <img
-              src="//image.idus.com/image/files/61e7b33b950a477b9723eb7411ead0d4_512.jpg"
-              class="hidden"
-              loading="lazy"
-              data-v-24b1dfb3=""
-            />
-          </div>
-        </div>
-        <div
-          class="BaseImage flex-[1_33%]"
-          style="
-            --BaseImage-width: 0;
-            --BaseImage-height: 0;
-            --BaseImage-radius: 0;
-          "
-          data-v-24b1dfb3=""
-          data-v-7ab1112c=""
-        >
-          <div
-            class="BaseImage__image"
-            style="
-              background-image: url(//image.idus.com/image/files/94214c56b5c7434a9e7ea5e2ad0f53b4_512.jpg);
-            "
-            data-v-24b1dfb3=""
-          >
-            <img
-              src="//image.idus.com/image/files/94214c56b5c7434a9e7ea5e2ad0f53b4_512.jpg"
-              class="hidden"
-              loading="lazy"
-              data-v-24b1dfb3=""
-            />
-          </div>
-        </div>
-        <!--]-->
+          <!--]-->
+        </button>
       </div>
     </div>
+    <div
+      class="w-[100%] flex h-[198px] cursor-pointer"
+      data-v-7ab1112c=""
+      v-for="images in productList.productImages"
+      :key="images.idx"
+    >
+      <!--[-->
+      <div
+        class="BaseImage flex-[1_33%]"
+        style="
+          --BaseImage-width: 0;
+          --BaseImage-height: 0;
+          --BaseImage-radius: 0;
+        "
+        data-v-24b1dfb3=""
+        data-v-7ab1112c=""
+      >
+        <div
+          class="BaseImage__image"
+          style="background-image: url(${images.aterlierProfileImage})"
+          data-v-24b1dfb3=""
+        >
+          <img
+            :src="images.productImageUrl"
+            class="hidden"
+            loading="lazy"
+            data-v-24b1dfb3=""
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 
-    <div style="display: none" class="PaginationMore" data-v-487afb77="">
-      <!---->
-    </div>
+  <div style="display: none" class="PaginationMore" data-v-487afb77="">
+    <!---->
   </div>
 </template>
 
   <script>
-import { defineComponent, computed } from "vue";
-import { useRouter } from "vue-router";
-import { useSideBarStore } from "@/stores/useSidebarStore";
-import ProductListComponent from "../product/ProductList4LayoutComponent.vue";
-import BaseTabsComponent from "./BaseTabComponent.vue";
-
+import { defineComponent } from "vue";
+import { useFollowStore } from "@/stores/useFollowStore";
+import { useAtelierStore } from "@/stores/useAtelierStore";
 export default defineComponent({
   name: "FollowAtelierListComponent",
-  components: {
-    ProductListComponent,
-    BaseTabsComponent,
-  },
   props: {
-    initialTab: {
-      type: Number,
-      default: 0,
+    productList: {
+      type: Object,
+      required: true,
     },
   },
+  computed: {
+    ...mapStores(useFollowStore),
+  },
   setup(props) {
-    const sideBarStore = useSideBarStore();
-    const router = useRouter();
+    const atelierStore = useAtelierStore();
 
-    const tabs = [
-      { id: 1, title: "찜한 작품", path: "/mypage/favorite/likes" },
-      {
-        id: 2,
-        title: "팔로우하는 작가",
-        path: "/mypage/favorite/follow-artist",
-      },
-      { id: 3, title: "최근 본 작품", path: "/mypage/favorite/recent-view" },
-    ];
-
-    //탭클릭시 탭변경함수
-    const handleTabClick = (index) => {
-      console.log(index);
-      sideBarStore.setActiveTab(index); // 사이드바 스토어 업데이트
-      router.push(tabs[index].path); // 경로 변경
-      console.log(sideBarStore.activeTab);
-      console.log("경로변경", tabs[index].path);
+    const clickFollowBtn = (props.productList.atelierIdx) => {
+      await atelierStore.clickFollowBtn(props.productList.atelierIdx);
     };
-
-    const activeTab = computed(() => sideBarStore.activeTab);
-
-    // 라우트가 바뀔 때 `activeTab` 동기화
-    sideBarStore.setActiveTab(props.initialTab);
-
     return {
-      tabs,
-      activeTab,
-      handleTabClick,
+      clickFollowBtn,
     };
   },
 });
 </script>
 
-  <style>
+  <style scoped>
+.DesktopFollowArtistItem[data-v-7ab1112c] {
+  align-items: center;
+  border-radius: 4px;
+  box-shadow: 0 0 4px #00000026;
+  display: flex;
+  margin-bottom: 10px;
+  overflow: hidden;
+  margin-top: 10px;
+}
 </style>
