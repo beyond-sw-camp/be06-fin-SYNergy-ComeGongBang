2b5f8c64432aca599e2969897a4c94be3c27756e
postrel63 <postrel63@naver.com>
2024-10-01 02:09:43 +0900

    [Feat] 이벤트 쿠폰 발행 예외처리  #53

--- backend/src/main/java/com/synergy/backend/global/common/BaseResponseStatus.java
+++ backend/src/main/java/com/synergy/backend/global/common/BaseResponseStatus.java
@@ -12,6 +12,7 @@ public enum BaseResponseStatus {
      * 필터 단계에서 확인되는 에러는 실제 에러코드 형태여야 함.
      */
     FAIL(false, 400, "요청 실패"),
+    UNAUTHORIZED(false, 401, "인증되지 않은 회원입니다."),
     EXPIRED_TOKEN(false, 500, "만료된 토큰 입니다."),
 
 
@@ -22,12 +23,15 @@ public enum BaseResponseStatus {
 
 
     /**
-     * 2000: 유저, 등급, 팔로우
+     * 2000: 유저, 등급, 팔로우, 쿠폰
      */
     NOT_FOUND_MEMBER(false, 2000, "회원을 찾을 수 없습니다."),
     REQUIRED_VALUE_NOT_ENTERED(false, 2001, "필수값이 모두 입력되지 않았습니다."),
     NEED_TO_LOGIN(false, 2002, "로그인이 필요한 서비스입니다."),
     ALREADY_EXIST_MEMBER(false,2003,"이미 존재하는 이메일입니다."),
+    COUPON_NOT_FOUND(false,2004, "쿠폰이 존재하지 않습니다."),
+    COUPON_SOLD_OUT(false,2004, "쿠폰이 모두 소진되었습니다. "),
+    COUPON_ISSUANCE_PERIOD_NOT(false,2004, "쿠폰 발급기간이 아닙니다."),
 
 
     /**
