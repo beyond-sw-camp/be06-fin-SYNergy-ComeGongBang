63fbbce54e67d58582f75319bc39716916e3c704
706com <ehdgusjdbswl@gmail.com>
2024-10-01 11:16:21 +0900

    [Feat] 공방 팔로우 응답 안에 공방 이미지들 정보 추가

--- backend/src/main/java/com/synergy/backend/domain/atelier/repository/AtelierProfileImagesRepository.java
+++ backend/src/main/java/com/synergy/backend/domain/atelier/repository/AtelierProfileImagesRepository.java
@@ -1,8 +1,17 @@
 package com.synergy.backend.domain.atelier.repository;
 
+import com.synergy.backend.domain.atelier.model.entity.Atelier;
 import com.synergy.backend.domain.atelier.model.entity.AtelierProfileImages;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AtelierProfileImagesRepository extends JpaRepository<AtelierProfileImages, Long> {
 
+    @Query("SELECT api.imageUrl "
+            + "FROM AtelierProfileImages api "
+            + "LEFT JOIN Atelier a ON api.atelier = a "
+            + "WHERE api.atelier = :atelier")
+    List<String> findAllByAtelier(Atelier atelier);
 }
