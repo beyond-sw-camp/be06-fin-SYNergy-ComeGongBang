c9f3bea7ed85ff462e223fb871dad6f562b95945
choijw1116 <choijw1116@gmail.com>
2024-10-02 14:33:03 +0900

    색변경

--- frontend/src/components/member/DeliveryComponent.vue
+++ frontend/src/components/member/DeliveryComponent.vue
@@ -6,43 +6,43 @@
         <div>
           <!--배송지 추가 버튼-->
           <button
-              @click="openModal"
-              data-v-524f63ea=""
-              data-v-7940d6dd=""
-              type="outline"
-              class="CoreButton BaseButtonRectangle body1-bold-small BaseButtonRectangle__outline"
-              style="
-                background-color: rgb(255, 255, 255);
-                color: rgb(239, 112, 20);
-                height: 40px;
-                flex-direction: row;
-                --core-button-padding-x: 16;
-                --button-rectangle-border-color: #ef7014;
-              "
+            @click="openModal"
+            data-v-524f63ea=""
+            data-v-7940d6dd=""
+            type="outline"
+            class="CoreButton BaseButtonRectangle body1-bold-small BaseButtonRectangle__outline"
+            style="
+              background-color: rgb(255, 255, 255);
+              color: rgb(239, 112, 20);
+              height: 40px;
+              flex-direction: row;
+              --core-button-padding-x: 16;
+              --button-rectangle-border-color: #ef7014;
+            "
           >
             <!---->
             <svg
-                data-v-6d2bd019=""
-                data-v-524f63ea=""
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                class="BaseIcon CoreButton__icon"
-                style="
-                  width: 18px;
-                  height: 18px;
-                  opacity: 1;
-                  fill: currentcolor;
-                  --BaseIcon-color: #333333;
-                  margin-right: 2px;
-                "
+              data-v-6d2bd019=""
+              data-v-524f63ea=""
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              class="BaseIcon CoreButton__icon"
+              style="
+                width: 18px;
+                height: 18px;
+                opacity: 1;
+                fill: currentcolor;
+                --BaseIcon-color: #333333;
+                margin-right: 2px;
+              "
             >
               <g clip-path="url(#clip0_124_2960)">
                 <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M12.75 5V11.25H19V12.75H12.75V19H11.25V12.75H5V11.25H11.25V5H12.75Z"
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M12.75 5V11.25H19V12.75H12.75V19H11.25V12.75H5V11.25H11.25V5H12.75Z"
                 ></path>
               </g>
               <defs>
@@ -52,24 +52,23 @@
               </defs>
             </svg>
             <div data-v-524f63ea="" class="inline-flex items-center">
-                <span data-v-524f63ea="" class="CoreButton__text"
+              <span data-v-524f63ea="" class="CoreButton__text"
                 >배송지 추가하기</span
-                >
-            </div>
-          </button
+              >
+            </div></button
           ><!---->
         </div>
         <AddAddressComponent
-            v-if="isModalVisible"
-            @close="closeModal"
-            @address-added="fetchAddresses"
+          v-if="isModalVisible"
+          @close="closeModal"
+          @address-added="fetchAddresses"
         />
       </div>
       <!--      배송지 없을 때, 띄워주는 div-->
       <div
-          class="flex flex-col items-center justify-center h-[640px]"
-          button-width="328"
-          style="display: none"
+        class="flex flex-col items-center justify-center h-[640px]"
+        button-width="328"
+        style="display: none"
       >
         <div class="subtitle3_bold_medium">등록된 배송지가 없습니다.</div>
         <div class="body1_regular_medium mt-[8px] mb-[16px]">
@@ -77,42 +76,42 @@
         </div>
         <div>
           <button
-              data-v-524f63ea=""
-              data-v-7940d6dd=""
-              type="outline"
-              class="CoreButton CoreButton--block BaseButtonRectangle body1-bold-small BaseButtonRectangle__outline"
-              style="
-                background-color: rgb(255, 255, 255);
-                color: rgb(239, 112, 20);
-                height: 40px;
-                flex-direction: row;
-                --core-button-padding-x: 16;
-                --button-rectangle-border-color: #ef7014;
-              "
+            data-v-524f63ea=""
+            data-v-7940d6dd=""
+            type="outline"
+            class="CoreButton CoreButton--block BaseButtonRectangle body1-bold-small BaseButtonRectangle__outline"
+            style="
+              background-color: rgb(255, 255, 255);
+              color: rgb(239, 112, 20);
+              height: 40px;
+              flex-direction: row;
+              --core-button-padding-x: 16;
+              --button-rectangle-border-color: #ef7014;
+            "
           >
             <!---->
             <svg
-                data-v-6d2bd019=""
-                data-v-524f63ea=""
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                class="BaseIcon CoreButton__icon"
-                style="
-                  width: 18px;
-                  height: 18px;
-                  opacity: 1;
-                  fill: currentcolor;
-                  --BaseIcon-color: #333333;
-                  margin-right: 2px;
-                "
+              data-v-6d2bd019=""
+              data-v-524f63ea=""
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              class="BaseIcon CoreButton__icon"
+              style="
+                width: 18px;
+                height: 18px;
+                opacity: 1;
+                fill: currentcolor;
+                --BaseIcon-color: #333333;
+                margin-right: 2px;
+              "
             >
               <g clip-path="url(#clip0_124_2960)">
                 <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M12.75 5V11.25H19V12.75H12.75V19H11.25V12.75H5V11.25H11.25V5H12.75Z"
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M12.75 5V11.25H19V12.75H12.75V19H11.25V12.75H5V11.25H11.25V5H12.75Z"
                 ></path>
               </g>
               <defs>
@@ -122,37 +121,40 @@
               </defs>
             </svg>
             <div data-v-524f63ea="" class="inline-flex items-center">
-                <span data-v-524f63ea="" class="CoreButton__text"
+              <span data-v-524f63ea="" class="CoreButton__text"
                 >배송지 추가하기</span
-                >
-            </div>
-          </button
+              >
+            </div></button
           ><!---->
         </div>
       </div>
 
       <div
-          class="flex flex-col w-full rounded-sm border px-[16px] pt-[20px] pb-[8px] mb-[12px] last:mt-0"
+        class="flex flex-col w-full rounded-sm border px-[16px] pt-[20px] pb-[8px] mb-[12px] last:mt-0"
       >
         <!-- 요게 반복       -->
         <div
-            v-for="address in deliveryStore.addresses"
-            :key="address.deliveryAddressIdx"
-            style="border: 1px solid rgb(229, 231, 235); padding: 20px 8px 16px 16px; margin-bottom: 12px"
-            class="flex flex-col w-full"
+          v-for="address in deliveryStore.addresses"
+          :key="address.deliveryAddressIdx"
+          style="
+            border: 1px solid rgb(229, 231, 235);
+            padding: 20px 8px 16px 16px;
+            margin-bottom: 12px;
+          "
+          class="flex flex-col w-full"
         >
           <div class="w-full flex flex-col gap-[8px]">
             <div class="flex">
               <!--              기본 배송지 일 떄 기본 배송지 태그 붙여줌-->
               <div
-                  v-if="address.isDefault"
-                  data-v-24a9185e=""
-                  class="BaseBadgeBusiness BaseBadgeBusiness__colorType--orange-500 BaseBadgeBusiness__size--medium mr-[4px] flex-none"
-                  style="
-                    font-weight: bold;
-                    color: rgb(239, 112, 20);
-                    background-color: rgb(255, 247, 242);
-                  "
+                v-if="address.isDefault"
+                data-v-24a9185e=""
+                class="BaseBadgeBusiness BaseBadgeBusiness__colorType--black-500 BaseBadgeBusiness__size--medium mr-[4px] flex-none"
+                style="
+                  font-weight: bold;
+                  color: #f5f5f5;
+                  background-color: #000;
+                "
               >
                 <!---->
                 기본배송지
@@ -180,19 +182,19 @@
           <div class="flex justify-end mt-[4px]">
             <!--            삭제 버튼-->
             <button
-                @click="deleteDeliveryAddress(address.id)"
-                data-v-524f63ea=""
-                data-v-8493c3f2=""
-                type="default"
-                class="CoreButton BaseButtonText body3-regular-small"
-                style="
-                  background-color: transparent;
-                  color: rgb(102, 102, 102);
-                  height: 30px;
-                  flex-direction: row-reverse;
-                  --core-button-padding-x: 8;
-                  font-weight: bold;
-                "
+              @click="deleteDeliveryAddress(address.id)"
+              data-v-524f63ea=""
+              data-v-8493c3f2=""
+              type="default"
+              class="CoreButton BaseButtonText body3-regular-small"
+              style="
+                background-color: transparent;
+                color: rgb(102, 102, 102);
+                height: 30px;
+                flex-direction: row-reverse;
+                --core-button-padding-x: 8;
+                font-weight: bold;
+              "
             >
               <!----><!---->
               <div data-v-524f63ea="" class="inline-flex items-center">
@@ -201,18 +203,18 @@
             </button>
             <!--            수정 버튼-->
             <button
-                data-v-524f63ea=""
-                data-v-8493c3f2=""
-                type="default"
-                class="CoreButton BaseButtonText body3-regular-small ml-[12px]"
-                style="
-                  background-color: transparent;
-                  color: rgb(102, 102, 102);
-                  height: 30px;
-                  flex-direction: row-reverse;
-                  --core-button-padding-x: 8;
-                  font-weight: bold;
-                "
+              data-v-524f63ea=""
+              data-v-8493c3f2=""
+              type="default"
+              class="CoreButton BaseButtonText body3-regular-small ml-[12px]"
+              style="
+                background-color: transparent;
+                color: rgb(102, 102, 102);
+                height: 30px;
+                flex-direction: row-reverse;
+                --core-button-padding-x: 8;
+                font-weight: bold;
+              "
             >
               <!----><!---->
               <div data-v-524f63ea="" class="inline-flex items-center">
@@ -228,9 +230,9 @@
 </template>
 
 <script>
-import {onMounted, ref} from 'vue';
-import {useDeliveryStore} from '@/stores/useDeliveryStore';
-import AddAddressComponent from './AddAddressComponent.vue';
+import { onMounted, ref } from "vue";
+import { useDeliveryStore } from "@/stores/useDeliveryStore";
+import AddAddressComponent from "./AddAddressComponent.vue";
 
 export default {
   components: {
