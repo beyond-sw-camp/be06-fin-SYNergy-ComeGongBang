53b895f88bf1f849688d1f823c2febc179399bfc
postrel63 <postrel63@naver.com>
2024-10-06 03:03:04 +0900

    [Feat] 불필요한 로그 삭제    #306

--- frontend/src/stores/useReviewStore.js
+++ frontend/src/stores/useReviewStore.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
-import { defineStore } from 'pinia';
+import axios from "axios";
+import { defineStore } from "pinia";
 
-export const useReviewStore = defineStore('review', {
+export const useReviewStore = defineStore("review", {
   state: () => ({
     review: {
-      content: '', // 후기 내용
+      content: "", // 후기 내용
       score: 0, // 후기 점수
       // images : []
     },
@@ -16,7 +16,7 @@ export const useReviewStore = defineStore('review', {
   actions: {
     // 후기 작성
     async createReview(review) {
-      let response = await axios.post('/api/review', review, {
+      let response = await axios.post("/api/review", review, {
         withCredentials: true,
       });
       if (response.status === 200) {
@@ -40,14 +40,14 @@ export const useReviewStore = defineStore('review', {
         this.page = response.data.result.reviewList.page;
         this.avgScore = response.data.result.avgScore;
       } catch (error) {
-        console.error('Failed to fetch review List:', error);
+        console.error("Failed to fetch review List:", error);
       }
     },
 
     //내가 작성한 후기
     async fetchMyReviews(page = 0, size = 10) {
       try {
-        const response = await axios.get('/api/review/me', {
+        const response = await axios.get("/api/review/me", {
           params: {
             page,
             size,
@@ -57,10 +57,8 @@ export const useReviewStore = defineStore('review', {
 
         this.reviewList = response.data.result.content;
         this.page = response.data.result.page;
-        console.log('response:', this.reviewList);
-        console.log('response:', this.page);
       } catch (error) {
-        console.error('Failed to fetch review List:', error);
+        console.error("Failed to fetch review List:", error);
       }
     },
 
@@ -68,7 +66,6 @@ export const useReviewStore = defineStore('review', {
     async readDetailReview() {
       let url = `/api/review/detail?reviewIdx=${this.reviewIdx}`;
       let response = await axios.get(url, { withCredentials: true });
-      console.log(response);
       this.review.content = response.data.result.content;
       this.review.score = response.data.result.score;
     },
