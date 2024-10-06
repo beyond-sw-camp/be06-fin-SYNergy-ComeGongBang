e0b78a9c91fe12f66e213a7ff36dd93a5f8598f4
postrel63 <postrel63@naver.com>
2024-10-07 01:19:49 +0900

    [fix] 쿠폰 목록 조회 응답 객체 #45

---
+++ backend/src/main/java/com/synergy/backend/domain/coupon/model/request/CouponListRes.java
@@ -0,0 +1,32 @@
+package com.synergy.backend.domain.coupon.model.request;
+
+import com.synergy.backend.domain.coupon.model.entity.MemberCoupon;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CouponListRes {
+
+    private Long memberCouponIdx;
+    private String name;
+    private Integer discountPercent;
+    private LocalDateTime publicationDate;
+    private LocalDateTime expirationDate;
+
+    public static CouponListRes from(MemberCoupon memberCoupon) {
+        return CouponListRes.builder()
+                .memberCouponIdx(memberCoupon.getIdx())
+                .name(memberCoupon.getCoupon().getName())
+                .discountPercent(memberCoupon.getCoupon().getDiscountPercent())
+                .publicationDate(memberCoupon.getPublicationDate())
+                .expirationDate(memberCoupon.getExpirationDate())
+                .build();
+    }
+}
