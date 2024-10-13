e89f0765d7b6a179b4c980b18ddaef4f0ab698ab
706com <ehdgusjdbswl@gmail.com>
2024-10-13 17:30:06 +0900

    [Refactor,Style] 카테고리 조회 성능 개선 및 컨벤션 준수

--- backend/src/main/java/com/synergy/backend/domain/product/controller/CategoryController.java
+++ backend/src/main/java/com/synergy/backend/domain/product/controller/CategoryController.java
@@ -1,16 +1,19 @@
 package com.synergy.backend.domain.product.controller;
 
 import com.synergy.backend.domain.product.model.CategoryDto;
-import com.synergy.backend.domain.product.model.entity.Category;
 import com.synergy.backend.domain.product.service.CategoryService;
 import com.synergy.backend.global.common.BaseResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/categories")
 public class CategoryController {
 
@@ -20,27 +23,21 @@ public class CategoryController {
     // 대분류 카테고리 조회
     @GetMapping("/top")
     public BaseResponse<List<CategoryDto>> getTopCategories() {
-        List<CategoryDto> topCategories = categoryService.getTopCategories().stream()
-                .map(CategoryDto::toDto)
-                .toList();
+        List<CategoryDto> topCategories = categoryService.getTopCategories();
         return new BaseResponse<>(topCategories);
     }
 
     // 중분류 카테고리 조회
     @GetMapping("/middle/{topCategoryId}")
     public BaseResponse<List<CategoryDto>> getMiddleCategories(@PathVariable Long topCategoryId) {
-        List<CategoryDto> middleCategories = categoryService.getMiddleCategories(topCategoryId).stream()
-                .map(CategoryDto::toDto)
-                .toList();
+        List<CategoryDto> middleCategories = categoryService.getMiddleCategories(topCategoryId);
         return new BaseResponse<>(middleCategories);
     }
 
     // 소분류 카테고리 조회 (특정 중분류에 속한 소분류들)
     @GetMapping("/bottom/{middleCategoryId}")
     public BaseResponse<List<CategoryDto>> getBottomCategories(@PathVariable Long middleCategoryId) {
-        List<CategoryDto> bottomCategories = categoryService.getBottomCategories(middleCategoryId).stream()
-                .map(CategoryDto::toDto)  // 엔티티 -> DTO 변환
-                .toList();
+        List<CategoryDto> bottomCategories = categoryService.getBottomCategories(middleCategoryId);
         return new BaseResponse<>(bottomCategories);
     }
 }
