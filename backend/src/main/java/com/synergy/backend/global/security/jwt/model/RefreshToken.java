25b8d36b6b92930fdf80ac770585f5c724529de9
706com <ehdgusjdbswl@gmail.com>
2024-10-14 17:24:58 +0900

    [Refactor] Redis 도입으로 인한 기존 RDB Entity 삭제 보류

--- backend/src/main/java/com/synergy/backend/global/security/jwt/model/RefreshToken.java
+++ backend/src/main/java/com/synergy/backend/global/security/jwt/model/RefreshToken.java
@@ -1,21 +1,28 @@
 package com.synergy.backend.global.security.jwt.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
+//import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
-
+//TODO : 기존 RDB 방식 : 삭제
 @NoArgsConstructor
 @Getter
 @Builder
 @AllArgsConstructor
-@RedisHash(value = "refreshToken", timeToLive = 7 * 24 * 60 * 60 * 1000L)
+@Entity
+//@RedisHash(value = "refreshToken", timeToLive = 7 * 24 * 60 * 60 * 1000L)
 public class RefreshToken {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
     private String refreshToken;
     private String email;
 
--- backend/src/main/java/com/synergy/backend/global/security/jwt/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/synergy/backend/global/security/jwt/repository/RefreshTokenRepository.java
@@ -2,9 +2,11 @@ package com.synergy.backend.global.security.jwt.repository;
 
 import com.synergy.backend.global.security.jwt.model.RefreshToken;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
+//TODO : 기존 RDB 방식 : 삭제
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByEmail(String email);
 
     void deleteByEmail(String email);
