a0cca899acee4d96bf6e12630bd6a3e2f4a3eb8d
Jun Hong <rjseka223@gmail.com>
2024-10-15 09:32:41 +0900

    [CI/CD] 백엔드 jenkinsfile 수정

--- backend/src/main/java/com/synergy/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/synergy/backend/global/config/SecurityConfig.java
@@ -60,6 +60,8 @@ public class SecurityConfig {
         config.addAllowedOrigin("http://localhost:3001"); // 허용할 출처
         config.addAllowedOrigin("http://localhost:8080"); // 허용할 출처
         config.addAllowedOrigin("https://www.comegongbang.kro.kr"); // 허용할 출처
+        config.addAllowedOrigin("10.109.158.141"); // 허용할 출처
+        config.addAllowedOrigin("10.97.2.114"); // 허용할 출처
 
         config.addAllowedMethod("*"); // 허용할 메서드 (GET, POST, PUT 등)
         config.addAllowedHeader("*"); // 허용할 헤더
