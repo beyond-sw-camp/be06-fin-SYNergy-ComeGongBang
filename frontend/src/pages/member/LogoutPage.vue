34000d4189cc65567e9ef55e461e79a7968b3ae6
706com <ehdgusjdbswl@gmail.com>
2024-10-17 01:33:33 +0900

    [Feat] 로그아웃 리다이렉트 페이지 구현

---
+++ frontend/src/pages/member/LogoutPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button>로그아웃중...</button>
+  </div>
+</template>
+
+<script>
+import {mapStores} from "pinia/dist/pinia";
+import {useMemberStore} from "@/stores/useMemberStore";
+
+export default {
+  computed: {
+    ...mapStores(useMemberStore)
+  },
+  async mounted(){
+    const memberStore = useMemberStore();
+    const response = await memberStore.logout();
+    if(response === true){
+      window.location.href = "/login";
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
