09a528f8f7d5c968a5b652e406fee93591c3fc48
706com <ehdgusjdbswl@gmail.com>
2024-10-21 08:12:13 +0900

    [Design] 메인페이지 글씨, 핫딜 기간 수정

--- frontend/src/components/common/CountDown.vue
+++ frontend/src/components/common/CountDown.vue
@@ -5,8 +5,9 @@
       :time="countdownTime"
       :transform="transformSlotProps" 
       v-slot="{ hours, minutes, seconds }"
+      class="hotdeal"
     >
-    {{ hours }}시간, {{ minutes }}분, {{ seconds }}초
+    {{ days }}일, {{ hours }}시간, {{ minutes }}분, {{ seconds }}초
     </vue-countdown>
   </span>
 </template>
@@ -23,10 +24,16 @@ export default {
       countdownTime: this.getTimeUntilMidnight(),
     };
   },
+  computed: {
+    // 남은 시간을 일로 변환하는 계산 속성
+    days() {
+      return Math.floor(this.countdownTime / (1000 * 60 * 60 * 24));
+    }
+  },
   methods: {
     getTimeUntilMidnight() {
       const now = new Date();
-      const tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1); // 내일 00시
+      const tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 4); // 내일 00시
       return tomorrow - now; // 내일 00시까지 남은 시간을 밀리초로 반환
     },
     // 카운트다운의 숫자가 10보다 작을 경우 앞에 0을 붙이는 함수
@@ -42,3 +49,9 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.hotdeal{
+  color : #de3c3c;
+}
+</style>
