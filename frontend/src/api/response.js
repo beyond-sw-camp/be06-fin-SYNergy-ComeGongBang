2a0e0e582222b92fda483b16115a4021831aa68d
postrel63 <postrel63@naver.com>
2024-10-21 15:56:46 +0900

    [Feat] 상품, 작가 페이지 404 #427

--- frontend/src/api/response.js
+++ frontend/src/api/response.js
@@ -11,7 +11,7 @@ axios.interceptors.response.use(
         if (error.response) {
             const status = error.response.status;
             if (status === 404) {
-                router.push("/error");
+                router.push("/notFound")
             }
             if (status === 401) {
                 const redirectUrl = error.response.data.redirectUrl;
